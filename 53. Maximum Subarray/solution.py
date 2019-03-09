f4f5b29c495a6cd3264f12b534e7e957d1973110
wilwfy <46659986+wilwfy@users.noreply.github.com>
2019-03-08 23:45:12 -0800

    Add solution from other people

--- 53. Maximum Subarray/solution.py
+++ 53. Maximum Subarray/solution.py
@@ -1,3 +1,14 @@
+# Solution from other people
+class Solution:
+    def maxSubArray(self, nums: 'List[int]') -> 'int':
+        max_sum = cur_sum = nums[0]
+        for num in nums[1:]:
+            cur_sum = max(num, cur_sum+num)
+            max_sum = max(max_sum, cur_sum)
+        return max_sum
+
+
+# Brute force solution
 class Solution:
     def maxSubArray(self, nums: 'List[int]') -> 'int':
         max = nums[0]
