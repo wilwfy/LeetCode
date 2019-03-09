dfc8a280b35a784933b136b7f56d5edb8f14e70b
wilwfy <46659986+wilwfy@users.noreply.github.com>
2019-03-09 01:09:08 -0800

    Updated comment for dynamic programming solution.

--- 53. Maximum Subarray/solution.py
+++ 53. Maximum Subarray/solution.py
@@ -1,4 +1,10 @@
 # Solution from other people
+# It uses a dynamic programming method explained in this video:
+# https://www.youtube.com/watch?v=2MmGzdiKR9Y
+# In general, when you move to a new item in the list/array,
+# you suppose if the maximum subarray is ended there, then which
+# would be a better choice to just keep current new item or to
+# include the previous solution which has been previously made.
 class Solution:
     def maxSubArray(self, nums: 'List[int]') -> 'int':
         max_sum = cur_sum = nums[0]
