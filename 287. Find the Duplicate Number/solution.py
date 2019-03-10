5f6965d42065e0e67a1bac9ee0b6912daa62cb58
wilwfy <46659986+wilwfy@users.noreply.github.com>
2019-03-10 01:47:58 -0800

    Create solution.py

---
+++ 287. Find the Duplicate Number/solution.py
@@ -0,0 +1,9 @@
+class Solution:
+    def findDuplicate(self, nums: List[int]) -> int:
+        dict = {}
+        for i in range(len(nums)):
+            if nums[i] in dict:
+                return nums[i]
+            else:
+                dict[nums[i]] = i
+        print("Error!")
