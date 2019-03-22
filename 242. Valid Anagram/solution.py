67e54a7a93e07e61a9db2c87c55b285ef588fb27
wilwfy <46659986+wilwfy@users.noreply.github.com>
2019-03-21 20:27:40 -0700

    Return false if length of strings are different

--- 242. Valid Anagram/solution.py
+++ 242. Valid Anagram/solution.py
@@ -1,5 +1,7 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
+        if (len(s) != len(t)):
+            return False
         dict = {}
         for a in s:
             if a not in dict:
