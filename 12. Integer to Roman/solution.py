e1e91ac4696454625b531730a1ee80ce03f73a40
wilwfy <46659986+wilwfy@users.noreply.github.com>
2019-03-23 16:03:41 -0700

    Update solution.py

--- 12. Integer to Roman/solution.py
+++ 12. Integer to Roman/solution.py
@@ -19,3 +19,30 @@ class Solution:
                 roman = roman + dict1[dec_pos] + dict1[dec_pos+1]
             dec_pos -= 1
         return roman
+
+    
+# Other's Solution
+# Make the key in Hash Table computable
+class Solution:
+    def intToRoman(self, num: int) -> str:
+        dic={1:"I", 5:"V", 10:"X", 50:"L", 100:"C", 500:"D", 1000:"M"}
+        a = num
+        b = 0
+        res = ""
+        r = 1
+	
+        while (a):
+            a, b = divmod(a,10)
+            if b>=1 and b<4:
+                res += dic[r] * b
+            elif b==4:
+                res += dic[5*r] + dic[r]
+            elif b==5:
+                res += dic[5*r]
+            elif 5<b and b<9:
+                res += dic[r]*(b-5) + dic[5*r]
+            elif b==9:
+                res += dic[10*r] + dic[r]
+            r = r*10
+        
+        return res[::-1]
