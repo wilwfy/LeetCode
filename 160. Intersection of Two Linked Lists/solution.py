7e2a618d03146f4b57ce90eb79b75a05f6f1096d
wilwfy <46659986+wilwfy@users.noreply.github.com>
2019-03-26 01:04:51 -0700

    Update solution.py

--- 160. Intersection of Two Linked Lists/solution.py
+++ 160. Intersection of Two Linked Lists/solution.py
@@ -4,6 +4,11 @@
 #         self.val = x
 #         self.next = None
 
+####################################
+# Hash Table solution
+# Time complexity : O(m+n)
+# Space complexity : O(m) or O(n) 
+####################################
 class Solution(object):
     def getIntersectionNode(self, headA, headB):
         """
@@ -27,3 +32,19 @@ class Solution(object):
                 return cur_B.next
             cur_B = cur_B.next
         return None
+
+
+####################################
+# Two Pointers solution
+# Time complexity : O(m+n)
+# Space complexity : O(1) 
+# Explanation:
+# 1. Maintain two pointers pApA and pBpB initialized at the head of A and B, respectively. 
+#    Then let them both traverse through the lists, one node at a time.
+# 2. When pApA reaches the end of a list, then redirect it to the head of B (yes, B, that's
+#    right.); similarly when pBpB reaches the end of a list, redirect it the head of A.
+# 3. If at any point pApA meets pBpB, then pApA/pBpB is the intersection node.
+# 4. If two lists have intersection, then their last nodes must be the same one. So when 
+#    pApA/pBpB reaches the end of a list, record the last element of A/B respectively. If
+#    the two last elements are not the same one, then the two lists have no intersections.
+####################################
