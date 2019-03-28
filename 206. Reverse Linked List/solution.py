f0cb9afdfbe793affdad66a5375d0bb32e572934
wilwfy <46659986+wilwfy@users.noreply.github.com>
2019-03-27 17:05:15 -0700

    Update solution.py
    Add other's solutions.

--- 206. Reverse Linked List/solution.py
+++ 206. Reverse Linked List/solution.py
@@ -42,3 +42,36 @@ class Solution:
         new_head, cur_node = self.recursiveReverseList(node.next)
         cur_node.next = ListNode(node.val)
         return new_head, cur_node.next
+
+
+#######################
+# Other's iterative solution
+#######################
+class Solution:
+# @param {ListNode} head
+# @return {ListNode}
+    def reverseList(self, head):
+        prev = None
+        while head:
+            curr = head
+            head = head.next
+            curr.next = prev
+            prev = curr
+        return prev
+
+
+#######################
+# Other's recursive solution
+#######################
+class Solution:
+# @param {ListNode} head
+# @return {ListNode}
+    def reverseList(self, head):
+        return self._reverse(head)
+
+    def _reverse(self, node, prev=None):
+        if not node:
+            return prev
+        n = node.next
+        node.next = prev
+        return self._reverse(n, node)
