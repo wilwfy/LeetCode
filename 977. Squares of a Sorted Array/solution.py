7fa912513d9bf9b94dbd1d8eb4ade02b0fd32f30
wilwfy <46659986+wilwfy@users.noreply.github.com>
2019-03-28 11:27:17 -0700

    Create solution.py

---
+++ 977. Squares of a Sorted Array/solution.py
@@ -0,0 +1,21 @@
+class Solution:
+    def sortedSquares(self, A: List[int]) -> List[int]:
+        return sorted(x*x for x in A)
+        
+        
+####################################
+# Other's Two Pointers solution
+####################################
+class Solution:
+    def sortedSquares(self, A):
+    	answer = [0] * len(A)
+    	l, r = 0, len(A) - 1
+    	while l <= r:
+    		left, right = abs(A[l]), abs(A[r])
+    		if left > right:
+    			answer[r - l] = left * left
+    			l += 1
+    		else:
+    			answer[r - l] = right * right
+    			r -= 1
+    	return answer
