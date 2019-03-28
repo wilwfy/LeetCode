a9081c41d4c1cd62e6df9971ad2b71ea5b645b85
wilwfy <46659986+wilwfy@users.noreply.github.com>
2019-03-27 21:58:48 -0700

    Update solution.py
    Add other's solution

--- 680. Valid Palindrome II/solution.py
+++ 680. Valid Palindrome II/solution.py
@@ -26,3 +26,26 @@ class Solution:
             l += 1
             r -= 1
         return True
+
+
+#####################
+# Other's solution
+#####################
+class Solution(object):
+    def validPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        # Time: O(n)
+        # Space: O(n)
+        left, right = 0, len(s) - 1
+        while left < right:
+            if s[left] != s[right]:
+                # take the two remaining substrings and compare against
+                # its reversed and see if either one is a palindrome.
+                one, two = s[left:right], s[left + 1:right + 1]
+                # The [::-1] gives a reversed list
+                return one == one[::-1] or two == two[::-1]
+            left, right = left + 1, right - 1
+        return True
