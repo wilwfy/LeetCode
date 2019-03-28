1ecb61521227d741371209839183081b310cf20f
wilwfy <46659986+wilwfy@users.noreply.github.com>
2019-03-27 20:59:24 -0700

    Update solution.py

--- 125. Valid Palindrome/solution.py
+++ 125. Valid Palindrome/solution.py
@@ -30,3 +30,29 @@ class Solution:
                 i += 1
                 j -= 1
         return True
+
+
+########################
+# Other's solution 1
+########################
+class Solution:
+    def isPalindrome(self, s):
+        l, r = 0, len(s)-1
+        while l < r:
+            while l < r and not s[l].isalnum():
+                l += 1
+            while l <r and not s[r].isalnum():
+                r -= 1
+            if s[l].lower() != s[r].lower():
+                return False
+            l +=1; r -= 1
+        return True
+    
+
+########################
+# Other's solution 2
+########################
+class Solution:
+    def isPalindrome(self, s):
+        chk = [ch.lower() for ch in s if ch.isalnum()]
+        return chk == chk[::-1]
