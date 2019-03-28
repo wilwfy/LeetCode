e2650bf67df37ff72266ae34d093c4ea8a5328d4
wilwfy <46659986+wilwfy@users.noreply.github.com>
2019-03-27 22:08:16 -0700

    Update solution.py

--- 680. Valid Palindrome II/solution.py
+++ 680. Valid Palindrome II/solution.py
@@ -28,6 +28,23 @@ class Solution:
         return True
 
 
+##############################################
+# Official solution based on Greedy algorithm
+# Time Complexity: O(N)
+# Space Complexity: O(1). Only pointers were stored in memory.
+##############################################
+class Solution(object):
+    def validPalindrome(self, s):
+        def is_pali_range(i, j):
+            return all(s[k] == s[j-k+i] for k in range(i, j))
+
+        for i in xrange(len(s) / 2):
+            if s[i] != s[~i]:
+                j = len(s) - 1 - i
+                return is_pali_range(i+1, j) or is_pali_range(i, j-1)
+        return True
+
+
 #####################
 # Other's solution
 #####################
