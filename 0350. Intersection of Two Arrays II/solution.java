fc43345361a7ef30de178b487176b9e782a681b6
wilwfy <46659986+wilwfy@users.noreply.github.com>
2019-04-03 21:28:41 -0700

    Add further thought about the follow-up question

--- 0350. Intersection of Two Arrays II/solution.java
+++ 0350. Intersection of Two Arrays II/solution.java
@@ -1,3 +1,13 @@
+/*
+ * What if elements of nums2 are stored on disk, and the memory is such that you
+ * cannot load all elements into the memory at once?
+ * If only nums2 cannot fit in memory, put all elements of nums1 into a HashMap, 
+ * read chunks of array that fit into the memory, and record the intersections.
+ * If both nums1 and nums2 are so huge that neither fit into the memory, sort them
+ * individually (external sort), then read 2 elements from each array at a time in
+ * memory, record intersections.
+ */
+
 class Solution {
     public int[] intersect(int[] nums1, int[] nums2) {
         if (nums1.length > nums2.length) {
