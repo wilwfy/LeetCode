e3aeaa4c2c5bab80413eff6a1e812ee6804baf86
wilwfy <46659986+wilwfy@users.noreply.github.com>
2019-04-05 01:33:31 -0700

    Add iterating method by using Stack

--- 0094. Binary Tree Inorder Traversal/solution.java
+++ 0094. Binary Tree Inorder Traversal/solution.java
@@ -20,3 +20,23 @@ class Solution {
         return inorder_nodes;
     }
 }
+
+// Iterating method using Stack
+class Solution {
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> res_nodes = new ArrayList<>();
+        Stack<TreeNode> parent_nodes = new Stack<>();
+        TreeNode cur_node = root;
+        if (cur_node == null) return res_nodes;
+        while ((cur_node != null) || (!parent_nodes.isEmpty())) {
+            while (cur_node != null) {
+                parent_nodes.push(cur_node);
+                cur_node = cur_node.left;
+            }
+            cur_node = parent_nodes.pop();
+            res_nodes.add(cur_node.val);
+            cur_node = cur_node.right;
+        }      
+        return res_nodes;
+    }
+}
