a8bd152c5162b199e2cb9e2a4cc488dd5f96767f
wilwfy <46659986+wilwfy@users.noreply.github.com>
2019-04-06 17:37:23 -0700

    Update solution.java

--- 0009. Palindrome Number/solution.java
+++ 0009. Palindrome Number/solution.java
@@ -9,3 +9,26 @@ class Solution {
         return true;
     }
 }
+
+
+/*
+ * Solution for the follow up without converting Integer to String
+ */
+class Solution {
+    public boolean isPalindrome(int x) {
+        if (x < 0) return false;
+        if (x == 0) return true;
+        List nums = new ArrayList<Integer>();
+        while (x > 0) {
+            nums.add(x%10);
+            x /= 10;
+        }
+        //System.out.println("size = " + size);
+        int size = nums.size();
+        for (int i = 0; i < size/2; i++) {
+            if (nums.get(i) != nums.get(size - 1 - i))
+                return false;
+        }
+        return true;
+    }
+}
