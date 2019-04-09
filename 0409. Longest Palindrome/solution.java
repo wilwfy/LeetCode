def3148ba59dfc21e894d3d4768a01afbb5f4ec7
wilwfy <46659986+wilwfy@users.noreply.github.com>
2019-04-09 00:42:46 -0700

    Add solution based on greedy algorithm

--- 0409. Longest Palindrome/solution.java
+++ 0409. Longest Palindrome/solution.java
@@ -20,3 +20,31 @@ class Solution {
     }
 }
 
+
+/*
+ * Official's solution: Greedy
+ *
+ * For each letter, say it occurs v times. We know we have v // 2 * 2 letters
+ * that can be partnered for sure. For example, if we have 'aaaaa', then we 
+ * could have 'aaaa' partnered, which is 5 // 2 * 2 = 4 letters partnered.
+ *
+ * At the end, if there was any v % 2 == 1, then that letter could have been
+ * a unique center. Otherwise, every letter was partnered. To perform this check,
+ * we will check for v % 2 == 1 and ans % 2 == 0, the latter meaning we haven't
+ * yet added a unique center to the answer.
+ */
+class Solution {
+    public int longestPalindrome(String s) {
+        int[] count = new int[128];
+        for (char c: s.toCharArray())
+            count[c]++;
+
+        int ans = 0;
+        for (int v: count) {
+            ans += v / 2 * 2;
+            if (ans % 2 == 0 && v % 2 == 1)
+                ans++;
+        }
+        return ans;
+    }
+}
