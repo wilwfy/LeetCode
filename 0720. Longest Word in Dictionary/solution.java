60e7723d142646052ba89a5a0c068e9402d3b06b
wilwfy <46659986+wilwfy@users.noreply.github.com>
2019-04-14 16:19:02 -0700

    Add official's solution based on Trie and DFS

--- 0720. Longest Word in Dictionary/solution.java
+++ 0720. Longest Word in Dictionary/solution.java
@@ -53,3 +53,92 @@ class Solution {
         return "";
     }
 }
+
+
+/*
+ * Other's sorting solution
+ */
+class Solution {
+    public String longestWord(String[] words) {
+        Arrays.sort(words);
+        Set<String> built = new HashSet<String>();
+        String res = "";
+        for (String w : words) {
+            if (w.length() == 1 || built.contains(w.substring(0, w.length() - 1))) {
+                res = w.length() > res.length() ? w : res;
+                built.add(w);
+            }
+        }
+        return res;
+    }
+}
+
+
+/*
+ * Official's solution by using Trie + Depth-First Search
+ *
+ * Time Complexity: O(∑wi), where wi is the length of words[i]. This is the complexity to build the trie and to search it.
+ *
+ * If we used a BFS instead of a DFS, and ordered the children in an array, we could drop the need to check whether the
+ * candidate word at each node is better than the answer, by forcing that the last node visited will be the best answer.
+ *
+ * Space Complexity: O(∑wi), the space used by our trie.
+ */
+class Solution {
+    public String longestWord(String[] words) {
+        Trie trie = new Trie();
+        int index = 0;
+        for (String word: words) {
+            trie.insert(word, ++index); //indexed by 1
+        }
+        trie.words = words;
+        return trie.dfs();
+    }
+}
+class Node {
+    char c;
+    HashMap<Character, Node> children = new HashMap();
+    int end;
+    public Node(char c){
+        this.c = c;
+    }
+}
+
+class Trie {
+    Node root;
+    String[] words;
+    public Trie() {
+        root = new Node('0');
+    }
+
+    public void insert(String word, int index) {
+        Node cur = root;
+        for (char c: word.toCharArray()) {
+            cur.children.putIfAbsent(c, new Node(c));
+            cur = cur.children.get(c);
+        }
+        cur.end = index;
+    }
+
+    public String dfs() {
+        String ans = "";
+        Stack<Node> stack = new Stack();
+        stack.push(root);
+        while (!stack.empty()) {
+            Node node = stack.pop();
+            if (node.end > 0 || node == root) {
+                if (node != root) {
+                    String word = words[node.end - 1];
+                    if (word.length() > ans.length() ||
+                            word.length() == ans.length() && word.compareTo(ans) < 0) {
+                        ans = word;
+                    }
+                }
+                for (Node nei: node.children.values()) {
+                    stack.push(nei);
+                }
+            }
+        }
+        return ans;
+    }
+}
