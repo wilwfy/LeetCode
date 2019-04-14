9fc97893c1e107e6a4e9fe0212128c1fec594cac
wilwfy <46659986+wilwfy@users.noreply.github.com>
2019-04-13 20:02:45 -0700

    Add other's solution based on directed edge

--- 0997. Find the Town Judge/solution.java
+++ 0997. Find the Town Judge/solution.java
@@ -40,3 +40,26 @@ class Solution {
         return -1;
     }
 }
+
+
+/*
+ * Other's solution
+ *
+ * Consider trust as a graph, all pairs are directed edge.
+ * The point with in-degree - out-degree = N - 1 become the judge.
+ *
+ * Time Complexity: Time O(T + N), space O(N)
+ */
+class Solution {
+    public int findJudge(int N, int[][] trust) {
+        int[] count = new int[N+1];
+        for (int[] t: trust) {
+            count[t[0]]--;
+            count[t[1]]++;
+        }
+        for (int i = 1; i <= N; ++i) {
+            if (count[i] == N - 1) return i;
+        }
+        return -1;
+    }
+}
