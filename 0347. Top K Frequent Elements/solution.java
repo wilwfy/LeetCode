a17dca0df64de75ffbd069664e4f7c94ca31ad55
wilwfy <46659986+wilwfy@users.noreply.github.com>
2019-04-15 17:34:27 -0700

    Update solution.java

--- 0347. Top K Frequent Elements/solution.java
+++ 0347. Top K Frequent Elements/solution.java
@@ -1,10 +1,16 @@
+/*
+ * Time complexity : O(Nlog(k)). The complexity of update the hash map is O(N).
+ *                   To build a heap and output list takes O(Nlog(k)). Hence the
+ *                   overall complexity of the algorithm is O(N+Nlog(k))=O(Nlog(k)).
+ * Space complexity : O(N) to store the hash map.
+ */
 class Solution {
     public List<Integer> topKFrequent(int[] nums, int k) {
         List<Integer> res = new ArrayList<>();
         Map<Integer, Integer> map = new HashMap<>();
         for (int num: nums)
             map.put(num, map.getOrDefault(num, 0) + 1);
-        // Max Heap - keep the k largest values in the heap
+        // Min Heap - keep the k largest values in the heap
         //            with the smallest one on the top of heap
         PriorityQueue<Integer> heap = new PriorityQueue<Integer>(
             (num1, num2) -> map.get(num1) - map.get(num2)
