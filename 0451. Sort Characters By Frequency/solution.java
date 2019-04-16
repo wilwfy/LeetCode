6dc411c217e0c79820348163e1e494ec4702e3bf
wilwfy <46659986+wilwfy@users.noreply.github.com>
2019-04-16 13:27:26 -0700

    Update solution.java

--- 0451. Sort Characters By Frequency/solution.java
+++ 0451. Sort Characters By Frequency/solution.java
@@ -17,3 +17,26 @@ class Solution {
         return res.toString();
     }
 }
+
+
+/*
+ * Other's solution
+ */
+public class Solution {
+    public String frequencySort(String s) {
+        Map<Character, Integer> map = new HashMap<>();
+        for (char c : s.toCharArray())
+            map.put(c, map.getOrDefault(c, 0) + 1);
+						
+        PriorityQueue<Map.Entry<Character, Integer>> pq = new PriorityQueue<>((a, b) -> b.getValue() - a.getValue());
+        pq.addAll(map.entrySet());
+				
+        StringBuilder sb = new StringBuilder();
+        while (!pq.isEmpty()) {
+            Map.Entry e = pq.poll();
+            for (int i = 0; i < (int)e.getValue(); i++) 
+                sb.append(e.getKey());
+        }
+        return sb.toString();
+    }
+}
