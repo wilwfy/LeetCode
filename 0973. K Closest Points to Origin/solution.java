c174a46aee60d83fe219fe8095d3f6eadc68672c
wilwfy <46659986+wilwfy@users.noreply.github.com>
2019-04-15 20:33:59 -0700

    Add other's solution besides Divide and Conquer

--- 0973. K Closest Points to Origin/solution.java
+++ 0973. K Closest Points to Origin/solution.java
@@ -20,6 +20,18 @@ class Solution {
 
 
 /*
+ * Time Complexity: O(N*logN).
+ * Space Complexity: O(1)
+ */
+class Solution {
+    public int[][] kClosest(int[][] points, int K) {
+        Arrays.sort(points, Comparator.comparing(p -> p[0] * p[0] + p[1] * p[1]));
+        return Arrays.copyOfRange(points, 0, K);
+    }
+}
+
+
+/*
  * Official solution based on Divide and Conquer
  *
  * Time Complexity: O(N) in average case complexity, where N is the length of points.
