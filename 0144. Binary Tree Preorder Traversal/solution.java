dd9ba0fa787dfe5433ef90353a735b6de16a5eb8
wilwfy <46659986+wilwfy@users.noreply.github.com>
2019-04-16 22:33:44 -0700

    Update solution.java

--- 0144. Binary Tree Preorder Traversal/solution.java
+++ 0144. Binary Tree Preorder Traversal/solution.java
@@ -27,8 +27,6 @@ class Solution {
  * Iteration solution by using Stack
  */
 class Solution {
-    //protected List<Integer> res = new ArrayList<>();
-    //protected Stack<TreeNode> stack = new Stack<>();
     public List<Integer> preorderTraversal(TreeNode root) {
         List<Integer> res = new ArrayList<>();
         Stack<TreeNode> stack = new Stack<>();
@@ -40,6 +38,32 @@ class Solution {
             if (cur != null) res.add(cur.val);
             if (cur.right != null) stack.push(cur.right);
             if (cur.left != null) stack.push(cur.left);
+        }
+        return res;
+    }
+}
+
+
+/*
+ * Other's iteration solution by using Stack
+ */
+class Solution {
+    public List<Integer> preorderTraversal(TreeNode root) {
+        List<Integer> res = new ArrayList<Integer>();
+        if (root == null) {
+            return res;
+        }
+        Deque<TreeNode> stack = new ArrayDeque<TreeNode>();
+        stack.push(root);
+        while (!stack.isEmpty()) {
+            TreeNode node = stack.pop();
+            res.add(node.val);
+            if (node.right != null) {
+                stack.push(node.right);
+            }
+            if (node.left != null) {
+                stack.push(node.left);
+            }
         }
         return res;
     }
