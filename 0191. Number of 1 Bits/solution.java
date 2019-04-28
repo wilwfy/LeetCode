af7fed4c46b7a8e5179340f42905bd80f5290dc5
wilwfy <46659986+wilwfy@users.noreply.github.com>
2019-04-27 23:59:42 -0700

    Update solution.java

--- 0191. Number of 1 Bits/solution.java
+++ 0191. Number of 1 Bits/solution.java
@@ -1,3 +1,7 @@
+/*
+ * The run time depends on the number of bits in nn. Because nn in this piece of code is a 32-bit integer, the time complexity is O(1).
+ * The space complexity is O(1), since no additional space is allocated.
+ */
 public class Solution {
     // you need to treat n as an unsigned value
     public int hammingWeight(int n) {
@@ -7,5 +11,24 @@ public class Solution {
             n >>= 1;
         }
         return res;
+    }
+}
+
+
+/*
+ * Official solution 2
+ *
+ * The run time depends on the number of 11-bits in nn. In the worst case, all bits in nn are 11-bits.
+ * In case of a 32-bit integer, the run time is O(1).
+ * The space complexity is O(1), since no additional space is allocated.
+ */
+public class Solution {
+    public int hammingWeight(int n) {
+        int sum = 0;
+        while (n != 0) {
+            sum++;
+            n &= (n - 1);
+        }
+        return sum;
     }
 }
