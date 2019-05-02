a1a8495599d1d114af4904ef21a5ba57f9415f39
wilwfy <46659986+wilwfy@users.noreply.github.com>
2019-05-02 00:07:37 -0700

    Add official solution

--- 0121. Best Time to Buy and Sell Stock/solution.java
+++ 0121. Best Time to Buy and Sell Stock/solution.java
@@ -19,3 +19,24 @@ class Solution {
         return best_profit;
     }
 }
+
+
+/*
+ * Official solution
+ * We need to find the largest peak following the smallest valley. We can maintain two
+ * variables - minprice and maxprofit corresponding to the smallest valley and maximum
+ * profit (maximum difference between selling price and minprice) obtained so far respectively.
+ */
+public class Solution {
+    public int maxProfit(int prices[]) {
+        int minprice = Integer.MAX_VALUE;
+        int maxprofit = 0;
+        for (int i = 0; i < prices.length; i++) {
+            if (prices[i] < minprice)
+                minprice = prices[i];
+            else if (prices[i] - minprice > maxprofit)
+                maxprofit = prices[i] - minprice;
+        }
+        return maxprofit;
+    }
+}
