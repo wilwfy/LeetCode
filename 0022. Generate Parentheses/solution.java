ca19d2f6ce72bf324c90ac582d0b1631894ee1f3
wilwfy <46659986+wilwfy@users.noreply.github.com>
2019-05-04 00:09:57 -0700

    Add official solution

--- 0022. Generate Parentheses/solution.java
+++ 0022. Generate Parentheses/solution.java
@@ -21,4 +21,31 @@ class Solution {
 }
 
 
+/*
+ * Official Solution
+ *
+ * We can start an opening bracket if we still have one (of n) left to place. And we can
+ * start a closing bracket if it would not exceed the number of opening brackets.
+ *
+ * Time Complexity : O((4^n)/sqrt(n)). Each valid sequence has at most n steps during the backtracking procedure.
+ * Space Complexity : O((4^n)/sqrt(n)), as described above, and using O(n) space to store the sequence. 
+ */
+class Solution {
+    public List<String> generateParenthesis(int n) {
+        List<String> ans = new ArrayList();
+        backtrack(ans, "", 0, 0, n);
+        return ans;
+    }
 
+    public void backtrack(List<String> ans, String cur, int open, int close, int max){
+        if (cur.length() == max * 2) {
+            ans.add(cur);
+            return;
+        }
+
+        if (open < max)
+            backtrack(ans, cur+"(", open+1, close, max);
+        if (close < open)
+            backtrack(ans, cur+")", open, close+1, max);
+    }
+}
