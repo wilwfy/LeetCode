f8c762cf3fb7a29f319fab78d0379942fcb7e4a0
wilwfy <46659986+wilwfy@users.noreply.github.com>
2019-05-12 17:22:31 -0700

    Add official solution

--- 0122. Best Time to Buy and Sell Stock II/solution.java
+++ 0122. Best Time to Buy and Sell Stock II/solution.java
@@ -16,3 +16,24 @@ class Solution {
         return profit;
     }
 }
+
+
+/*
+ * Official solution: Simple One Pass
+ *
+ * we can observe that the total profit is equal to the difference 
+ * between the heights of the consecutive peak and valley.
+ *
+ * Time complexity : O(n). Single pass.
+ * Space complexity : O(1). Constant space required.
+ */
+class Solution {
+    public int maxProfit(int[] prices) {
+        int maxprofit = 0;
+        for (int i = 1; i < prices.length; i++) {
+            if (prices[i] > prices[i - 1])
+                maxprofit += prices[i] - prices[i - 1];
+        }
+        return maxprofit;
+    }
+}
