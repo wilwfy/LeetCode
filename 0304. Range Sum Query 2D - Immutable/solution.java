fe4d6785635fa3fdf8142025a049224b67c77a7c
wilwfy <46659986+wilwfy@users.noreply.github.com>
2019-05-16 01:22:31 -0700

    Create solution.java

---
+++ 0304. Range Sum Query 2D - Immutable/solution.java
@@ -0,0 +1,35 @@
+/**
+ * Your NumMatrix object will be instantiated and called as such:
+ * NumMatrix obj = new NumMatrix(matrix);
+ * int param_1 = obj.sumRegion(row1,col1,row2,col2);
+ */
+
+/*
+ * Similar to the official solution: Caching Rows
+ *
+ * Time complexity : O(m) time per query, O(mn) time pre-computation.
+ *                   The pre-computation in the constructor takes O(mn) time.
+ *                   The sumRegion query takes O(m) time.
+ * Space complexity : O(mn). The algorithm uses O(mn) space to store the cumulative sum of all rows.
+ */
+class NumMatrix {
+    private int[][] sumMatrix;
+
+    public NumMatrix(int[][] matrix) {
+        if ((matrix == null) || (matrix.length == 0)) return;
+        sumMatrix = new int[matrix.length][matrix[0].length + 1];
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                sumMatrix[i][j+1] = sumMatrix[i][j] + matrix[i][j];
+            }
+        }
+    }
+    
+    public int sumRegion(int row1, int col1, int row2, int col2) {
+        int sum = 0;
+        for (int row = row1; row <= row2; row++) {
+            sum += sumMatrix[row][col2+1] - sumMatrix[row][col1];
+        }
+        return sum;
+    }
+}
