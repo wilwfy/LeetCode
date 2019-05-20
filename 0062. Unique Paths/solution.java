86f6955dbaef741fd7ffd6fb81ee3d7c086944ef
wilwfy <46659986+wilwfy@users.noreply.github.com>
2019-05-20 00:16:39 -0700

    My Dynamic Programming and other's Math solution.

--- 0062. Unique Paths/solution.java
+++ 0062. Unique Paths/solution.java
@@ -1,3 +1,6 @@
+/*
+ * Space Complexity: O(m*n)
+ */
 class Solution {
     public int uniquePaths(int m, int n) {
         if ((m == 0) || (n == 0)) return 0;
@@ -10,5 +13,62 @@ class Solution {
             }
         }
         return dp[m-1][n];
+    }
+}
+
+
+/*
+ * Use less memory
+ * Space Complexity: O(n)
+ */
+class Solution {
+    public int uniquePaths(int m, int n) {
+        if ((m == 0) || (n == 0)) return 0;
+        int[] dp_row = new int[n];
+        Arrays.fill(dp_row, 1);
+        for (int i = 1; i < m; i++) {
+            int dp_pre_col = 0;
+            for (int j = 0; j < n; j++) {
+                dp_row[j] = dp_pre_col + dp_row[j];
+                dp_pre_col = dp_row[j];
+            }
+        }
+        return dp_row[n-1];
+    }
+}
+
+
+/*
+ * Other's Math solution
+ *
+ * This is a combinatorial problem and can be solved without DP. For mxn grid, robot has to move exactly m-1 steps down
+ * and n-1 steps right and these can be done in any order. For the eg., given in question, 3x7 matrix, robot needs to
+ * take 2+6 = 8 steps with 2 down and 6 right in any order. That is nothing but a permutation problem. Denote down as
+ * 'D' and right as 'R', following is one of the path :   D R R R D R R R
+ * We have to tell the total number of permutations of the above given word. So, decrease both m & n by 1 and apply following
+ * formula:
+ * Total permutations = (m+n)! / (m! * n!)
+ * 
+ * Space Complexity: O(1)
+ */
+public class Solution {
+    public int uniquePaths(int m, int n) {
+        if(m == 1 || n == 1)
+            return 1;
+        m--;
+        n--;
+        if(m < n) {              // Swap, so that m is the bigger number
+            m = m + n;
+            n = m - n;
+            m = m - n;
+        }
+        long res = 1;
+        int j = 1;
+        for(int i = m+1; i <= m+n; i++, j++){       // Instead of taking factorial, keep on multiply & divide
+            res *= i;
+            res /= j;
+        }
+            
+        return (int)res;
     }
 }
