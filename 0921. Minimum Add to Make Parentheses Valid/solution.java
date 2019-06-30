f4f9c7a62f5873151703c9b5b3d9c73c280ff7c7
wilwfy <46659986+wilwfy@users.noreply.github.com>
2019-06-30 00:09:57 -0700

    Add other's solution

--- 0921. Minimum Add to Make Parentheses Valid/solution.java
+++ 0921. Minimum Add to Make Parentheses Valid/solution.java
@@ -20,3 +20,44 @@ class Solution {
         return resSize;
     }
 }
+
+
+/*
+ * Other's Solution
+ *
+ * Intuition:
+ * To make a string valid,
+ * we can add some ( on the left,
+ * and add some ) on the right.
+ * We need to find the number of each.
+ * 
+ * Explanation:
+ * left records the number of ( we need to add on the left of S.
+ * right records the number of ) we need to add on the left of S,
+ * which equals to the number of current opened parentheses.
+ * 
+ * Loop char c in the string S:
+ * if (c == '('), we increment right,
+ * if (c == ')'), we decrement right.
+ * When right is already 0, we increment left
+ * Return left + right in the end
+ * 
+ * Time Complexity:
+ * Time O(N)
+ * Space O(1)
+ */
+class Solution {
+    public int minAddToMakeValid(String S) {
+        int left = 0, right = 0;
+        for (int i = 0; i < S.length; ++i) {
+            if (S.charAt(i) == '(') {
+                right++;
+            } else if (right > 0) {
+                right--;
+            } else {
+                left++;
+            }
+        }
+        return left + right;
+    }
+}
