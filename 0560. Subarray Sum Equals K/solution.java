7c07ec3435725ae8268e6f66af8404ed26d51321
wilwfy <46659986+wilwfy@users.noreply.github.com>
2019-09-24 07:17:42 -0700

    Add soluting of using HashMap.

--- 0560. Subarray Sum Equals K/solution.java
+++ 0560. Subarray Sum Equals K/solution.java
@@ -18,3 +18,25 @@ class Solution {
         return res;
     }
 }
+
+/**
+ * Official's solution: Using HashMap
+ *
+ * Time complexity : O(n). The entire numsnums array is traversed only once.
+ * Space complexity : O(n). Hashmap mapmap can contain upto nn distinct entries in the worst case.
+ */
+class Solution {
+    public int subarraySum(int[] nums, int k) {
+        if ((nums == null) || (nums.length == 0)) return 0;
+        int cnt = 0;
+        int sum = 0;
+        Map<Integer, Integer> sumMap = new HashMap<>();
+        sumMap.put(0, 1); // Initialize the map of sum
+        for (int i = 0; i < nums.length; i++) {
+            sum += nums[i];
+            cnt += sumMap.getOrDefault(sum - k, 0);
+            sumMap.put(sum, sumMap.getOrDefault(sum, 0) + 1);
+        }
+        return cnt;
+    }
+}
