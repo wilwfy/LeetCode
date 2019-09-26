87f05a780c0dda8f195f1b6062df7ee81e2435fc
wilwfy <46659986+wilwfy@users.noreply.github.com>
2019-09-25 17:02:30 -0700

    Add official solution of using Two Pointers

--- 0713. Subarray Product Less Than K/solution.java
+++ 0713. Subarray Product Less Than K/solution.java
@@ -28,3 +28,30 @@ class Solution {
         return cnt;
     }
 }
+
+
+/**
+ * Official and Other's Solution: Sliding Window with Two Pointers
+ *
+ * The idea is always keep an max-product-window less than K;
+ * Every time shift window by adding a new number on the right(j), if the product is greater than k, 
+ * then try to reduce numbers on the left(i), until the subarray product fit less than k again, (subarray could be empty);
+ * Each step introduces x new subarrays, where x is the size of the current window (j + 1 - i);
+ *
+ * Time Complexity: O(N), where N is the length of nums. pointer2 can only be incremented at most N times.
+ * Space Complexity: O(1)
+ */
+class Solution {
+    public int numSubarrayProductLessThanK(int[] nums, int k) {
+        if (k == 0) return 0;
+        int cnt = 0, prod = 1;
+        for (int pointer1 = 0, pointer2 = 0; pointer1 < nums.length; pointer1++) {
+            prod *= nums[pointer1];
+            while (prod >= k && pointer2 <= pointer1) {
+                prod /= nums[pointer2++];
+            }
+            cnt += pointer1 - pointer2 + 1;
+        }
+        return cnt;
+    }
+}
