36c3817a03da308a32ad4b60f3c654d96808151d
wilwfy <46659986+wilwfy@users.noreply.github.com>
2019-09-27 07:20:18 -0700

    Add other's solution of using prefix sum that is faster than HashMap.

--- 0974. Subarray Sums Divisible by K/solution.java
+++ 0974. Subarray Sums Divisible by K/solution.java
@@ -20,3 +20,29 @@ class Solution {
         return cnt;
     }
 }
+
+/**
+ * Other's solution of using prefix sum and array
+ *
+ */
+class Solution {
+    public int subarraysDivByK(int[] A, int K) {
+        if (K == 0) return 0;
+        int cnt = 0, sum = 0;
+        int[] remainders = new int[K];
+        /*
+         * It's a common technique in prefix sum. Since sum[i, j] = presum[j] - presum[i - 1], how to calculate sum[0, j]?
+For current j, we need to know how many sum[i, j] is divisible by K. So i could be 0.
+So in prefix sum, we add 0 ahead. For example, A = [1,2,3,4], presum=[0,1,3,6,10] And now we can culculate every sum[i, j] through the equation.
+         */
+        remainders[0] = 1;
+        for (int i = 0; i < A.length; i++) {
+            sum += A[i];
+            sum %= K;
+            if (sum < 0) sum += K;
+            cnt += remainders[sum];
+            remainders[sum]++;
+        }
+        return cnt;
+    }
+}
