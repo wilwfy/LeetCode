f5e1fd66e5a38310be2195f412a61d733b22b198
wilwfy <46659986+wilwfy@users.noreply.github.com>
2019-10-22 22:50:24 -0700

    Add other's amazing concise solution.

---
+++ 0454. 4Sum II/solution.py
@@ -0,0 +1,6 @@
+'''
+Other's amazing concise solution.
+'''
+def fourSumCount(self, A, B, C, D):
+    AB = collections.Counter(a+b for a in A for b in B)
+    return sum(AB[-c-d] for c in C for d in D)
