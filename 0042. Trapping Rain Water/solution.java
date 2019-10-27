96689004d74ff140714cd20a2b86c520fdf64197
wilwfy <46659986+wilwfy@users.noreply.github.com>
2019-10-27 07:17:37 -0700

    Add official solution of using 2 pointers.

--- 0042. Trapping Rain Water/solution.java
+++ 0042. Trapping Rain Water/solution.java
@@ -47,9 +47,9 @@ class Solution {
  * Official solution by using Stack
  *
  * Intuition:
- *           Instead of storing the largest bar upto an index as in Approach 2, we can use stack to keep track of the
- *           bars that are bounded by longer bars and hence, may store water. Using the stack, we can do the calculations
- *           in only one iteration.
+ *           Instead of storing the largest bar upto an index as in Approach of Dynamic Programming, we can use stack to keep
+ *           track of the bars that are bounded by longer bars and hence, may store water. Using the stack, we can do the
+ *           calculations in only one iteration.
  *           We keep a stack and iterate over the array. We add the index of the bar to the stack if bar is smaller than
  *           or equal to the bar at top of stack, which means that the current bar is bounded by the previous bar in the
  *           stack. If we found a bar longer than that at the top, we are sure that the bar at the top of the stack is bounded
@@ -88,6 +88,59 @@ class Solution {
                 res += distance * bounded_height;
             }
             sta.push(current++);
+        }
+        return res;
+    }
+}
+
+
+/**
+ * Official solution by using 2 Pointers.
+ *
+ * Intuition:
+ *           As in Approach of Dynamic Programming, instead of computing the left and right parts seperately, we may think
+ *           of some way to do it in one iteration. From the figure in dynamic programming approach, notice that as long as
+ *           right_max[i]>left_max[i] (from element 0 to 6), the water trapped depends upon the left_max, and similar is the
+ *           case when left_max[i]>right_max[i] (from element 8 to 11). So, we can say that if there is a larger bar at one
+ *           end (say right), we are assured that the water trapped would be dependant on height of bar in current direction
+ *           (from left to right). As soon as we find the bar at other end (right) is smaller, we start iterating in opposite
+ *           direction (from right to left). We must maintain left_max and right_max during the iteration, but now we can do
+ *           it in one iteration using 2 pointers, switching between the two.
+ * Algorithm:
+ *           Initialize left pointer to 0 and right pointer to size-1
+ *           While left<right, do:
+ *               If height[left] is smaller than height[right]
+ *                   If height[left]≥left_max, update left_max
+ *                   Else add left_max−height[left] to ans
+ *                   Add 1 to left.
+ *               Else
+ *                   If height[right]≥right_max, update right_max
+ *                   Else add right_max−height[right] to ans
+ *                   Subtract 1 from right.
+ *
+ * Time complexity: O(n). Single iteration of O(n).
+ * Space complexity: O(1) extra space. Only constant space required for left, right, left_max and right_max..
+ */
+class Solution {
+    public int trap(int[] height) {
+        if ((height == null) || (height.length == 0)) return 0;
+        int res = 0;
+        int l = 0, r = height.length - 1;
+        int leftMax = 0, rightMax = 0;
+        while (l < r) {
+            if (height[l] < height[r]) {
+                if (height[l] >= leftMax)
+                    leftMax = height[l];
+                else
+                    res += leftMax - height[l];
+                l++;
+            } else {
+                if (height[r] >= rightMax)
+                    rightMax = height[r];
+                else
+                    res += rightMax - height[r];
+                r--;
+            }
         }
         return res;
     }
