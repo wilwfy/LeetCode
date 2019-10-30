c158078c2f2cf05ec02cd8c48e51dc8d5b2c5039
wilwfy <46659986+wilwfy@users.noreply.github.com>
2019-10-30 08:07:39 -0700

    Solution of Binary Search similar to Problem 0378

--- 0668. Kth Smallest Number in Multiplication Table/solution.java
+++ 0668. Kth Smallest Number in Multiplication Table/solution.java
@@ -36,3 +36,34 @@ class Node {
     }
 }
 
+
+/**
+ * Official solution of Binary Search which is similar to Problem 378 'Kth Smallest Element in a Sorted Matrix'
+ *
+ * Time Complexity: O(m∗log(m∗n)).
+ *                  Our binary search divides the interval [lo, hi] into half at each step. At each step, we call
+ *                  getLessEqual() which requires O(m) time.
+ * Space Complexity: O(1). We only keep integers in memory during our intermediate calculations.
+ */
+class Solution {
+    public int findKthNumber(int m, int n, int k) {
+        int lo = 1, hi = m * n;
+        while (lo < hi) {
+            int mi = lo + (hi - lo) / 2;
+            int cnt = getLessEqual(m, n, mi);
+            if (cnt < k)
+                lo = mi + 1;
+            else
+                hi = mi;
+        }
+        return lo;
+    }
+    
+    public int getLessEqual(int m, int n, int midVal) {
+        int count = 0;
+        for (int i = 1; i <= m; i++) {
+            count += Math.min(midVal/i, n);
+        }
+        return count;
+    }
+}
