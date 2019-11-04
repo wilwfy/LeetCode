197953df819249627abd1975311a293c6d0be20d
wilwfy <46659986+wilwfy@users.noreply.github.com>
2019-11-03 19:24:28 -0800

    Add by referring to other's solution of Priority Queue

---
+++ 0786. K-th Smallest Prime Fraction/solution.java
@@ -0,0 +1,30 @@
+/**
+ * Referring to other's solution of Priority Queue
+ * 
+ * Reference Post: https://leetcode.com/problems/k-th-smallest-prime-fraction/discuss/115486/Java-AC-O(max(nk)-*-logn)-Short-Easy-PriorityQueue/116025
+ */
+class Solution {
+    public int[] kthSmallestPrimeFraction(int[] A, int K) {
+        int n = A.length;
+        
+        // 0: numerator idx, 1: denominator idx
+        PriorityQueue<int[]> pq = new PriorityQueue<int[]>(
+            (a, b) -> A[a[0]] * A[b[1]] - A[b[0]] * A[a[1]]
+        );
+        
+        for (int i = 0; i < n - 1; i++) {
+            pq.offer(new int[]{i, n - 1});
+        }
+        
+        for (int i = 0; i < K - 1; i++) {
+            int[] fr = pq.poll();
+            int nuIndex = fr[0], deIndex = fr[1];
+            if (deIndex > nuIndex + 1) {
+                fr[1]--;
+                pq.offer(fr);
+            }
+        }
+        
+        return new int[]{A[pq.peek()[0]], A[pq.peek()[1]]};
+    }
+}
