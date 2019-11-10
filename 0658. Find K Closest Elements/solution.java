5b4fda0df0410134af97686a21fbb45c92339aed
wilwfy <46659986+wilwfy@users.noreply.github.com>
2019-11-10 03:11:21 -0800

    Add other's faster solution based on Binary Search

--- 0658. Find K Closest Elements/solution.java
+++ 0658. Find K Closest Elements/solution.java
@@ -69,3 +69,44 @@ class Solution {
         }
     }
 }
+
+
+/**
+ * Other's faster solution based on Binary Search
+ * https://leetcode.com/problems/find-k-closest-elements/discuss/106426/JavaC%2B%2BPython-Binary-Search-O(log(N-K)-%2B-K)
+ *
+ * Intuition:
+ *           The array is sorted. If we want find the one number closest to x, we don't have to check one by one.
+ *           it's straightforward to use binary research. Now we want the k closest, the logic should be similar.
+ * Explanation:
+ *           Assume we are taking A[i] ~ A[i + k -1]. We can binary research i.
+ *           We compare the distance between x - A[mid] and A[mid - k] - x
+ *           If x - A[mid] > A[mid + k] - x,
+ *           it means A[mid + 1] ~ A[mid + k] is better than A[mid] ~ A[mid + k - 1], and we have mid smaller than the right i.
+ *           So assign left = mid + 1.
+ *
+ * Note that, you SHOULD NOT compare the absolute value of abs(x - A[mid]) and abs(A[mid + k] - x).
+ * It fails at cases like A = [1,1,2,2,2,2,2,3,3], x = 3, k = 3
+ * The problem is interesting and good. Unfortunately the test cases is terrible. The worst part of Leetcode test cases is that,
+ * you sumbit a wrong solution but get accepted.
+ * You didn't read my post and upvote carefully, then you miss this key point.
+ *
+ * Time complexity : O(log(N - K)) to binary research and find reseult.
+ * Space complexity : O(K) to create the returned list.
+ */
+class Solution {
+    public List<Integer> findClosestElements(int[] arr, int k, int x) {       
+        int left = 0, right = arr.length - k;
+        while (left < right) {
+            int mid = (left + right) / 2;
+            if (x - arr[mid] > arr[mid + k] - x)
+                left = mid + 1;
+            else
+                right = mid;
+        }
+        
+        return Arrays.stream(arr, left, left + k)
+            .boxed()
+            .collect(Collectors.toList());
+    }
+}
