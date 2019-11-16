fdb6626bb327233c678bd1a6224767b9400e5994
wilwfy <46659986+wilwfy@users.noreply.github.com>
2019-11-16 00:26:28 -0800

    Add solutions of 1-D, 2-D Dynamic Programming

--- 0486. Predict the Winner/solution.java
+++ 0486. Predict the Winner/solution.java
@@ -56,3 +56,50 @@ class Solution {
         return memo[start][end];
     }
 }
+
+
+/**
+ * Official solution of 2-D Dynamic Programming
+ *
+ * Time complexity : O(n^2). ((n+1) x n)/2 entries in dp array of size (n+1) x n is filled once. Here, n refers to the length of nums array.
+ * Space complexity : O(n^2). dp array of size (n+1) x n is used.
+ */
+class Solution {
+    public boolean PredictTheWinner(int[] nums) {
+        // dp[i,j]= max(nums[i]−dp[i+1][j], nums[j]−dp[i][j−1])
+        int[][] dp = new int[nums.length + 1][nums.length];
+        
+        for (int start = nums.length; start >= 0; start--) {
+            for (int end = start + 1; end < nums.length; end++) {
+                int a = nums[start] - dp[start+1][end];
+                int b = nums[end] - dp[start][end-1];
+                dp[start][end] = Math.max(a, b);
+            }
+        }
+        
+        return dp[0][nums.length-1] >= 0;
+    }
+}
+
+
+/**
+ * Official solution of 2-D Dynamic Programming
+ *
+ * Time complexity : O(n^2). The elements of dp array are updated 1+2+3+...+n times. Here, n refers to the length of nums array.
+ * Space complexity : O(n). 1-D dp array of size n is used.
+ */
+class Solution {
+    public boolean PredictTheWinner(int[] nums) {
+        // dp[i,j]= max(nums[i]−dp[i+1][j], nums[j]−dp[i][j−1])
+        int[] dp = new int[nums.length];
+        
+        for (int start = nums.length; start >= 0; start--) {
+            for (int end = start + 1; end < nums.length; end++) {
+                int a = nums[start] - dp[end];
+                int b = nums[end] - dp[end-1];
+                dp[end] = Math.max(a, b);
+            }
+        }
+        return dp[nums.length-1] >= 0;
+    }
+}
