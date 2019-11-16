1dcbe5944118e43fa012a95c90e9114d6bd73ce7
wilwfy <46659986+wilwfy@users.noreply.github.com>
2019-11-15 23:50:08 -0800

    Add solution of using Recursion with Memoization

--- 0486. Predict the Winner/solution.java
+++ 0486. Predict the Winner/solution.java
@@ -23,3 +23,36 @@ class Solution {
         return turn * Math.max(turn * a, turn * b);
     }
 }
+
+/**
+ * Official solution of using Recursion with Memoization
+ *
+ * Time complexity : O(n^2). The entire memo array of size n x n is filled only once. Here, n refers to the size of nums array.
+ * Space complexity : O(n^2). memo array of size n x n is used for memoization.
+ */
+class Solution {
+    public boolean PredictTheWinner(int[] nums) {
+        // Use Integer[][] not int[][] because the calculation result
+        // could be 0 which is the default value of int[][] element,
+        // So we need use null as the default value.
+        Integer[][] memo = new Integer[nums.length][nums.length];
+        return winner(nums, 0, nums.length-1, memo) >= 0;
+    }
+    
+    public int winner(int[] nums, int start, int end, Integer[][] memo) {
+        if (start == end)
+            return nums[start];
+        
+        if (memo[start][end] != null)
+            return memo[start][end];
+        
+        // player select the number at head then substract the other player for next turn
+        int a = nums[start] - winner(nums, start+1, end, memo);
+        // player select the number at tail then substract the other player for next turn
+        int b = nums[end] - winner(nums, start, end-1, memo);
+        
+        // select the greater value for the player under the subarray from indices start~end
+        memo[start][end] = Math.max(a, b);
+        return memo[start][end];
+    }
+}
