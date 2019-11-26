bbef3d448d70d48f582347439d79abb27134ffc6
wilwfy <46659986+wilwfy@users.noreply.github.com>
2019-11-26 06:11:26 -0800

    Add Other's DP solution which is not good as Backtracking

--- 0039. Combination Sum/solution.java
+++ 0039. Combination Sum/solution.java
@@ -29,3 +29,47 @@ class Solution {
         }
     }
 }
+
+
+/**
+ * Other's solution of Dynamic Programming but performance is not good as the Backtracking
+ */
+class Solution {
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        // sort candidates to try them in asc order
+        Arrays.sort(candidates); 
+        // dp[t] stores all combinations that add up to t
+        List<List<Integer>>[] dp = new ArrayList[target+1];
+        
+        // build up dp
+        for(int t=0; t<=target; t++) {
+            // initialize
+            dp[t] = new ArrayList();
+            // initialize
+            List<List<Integer>> combList = new ArrayList();
+            
+            // for each t, find possible combinations
+            for(int j=0; j<candidates.length && candidates[j] <= t; j++) {
+                if(candidates[j] == t) {
+                    combList.add(Arrays.asList(candidates[j])); // itself can form a list
+                } else {
+                    for(List<Integer> prevlist: dp[t-candidates[j]]) { // here use our dp definition
+                        // The author thought it makes more sense to compare with the last element
+                        // only add to list when the candidates[j] >= the last element
+                        // so the list remains ascending order, can prevent duplicate (ex. has [2 3 3], no [3 2 3]),
+                        // if the current candidate is smaller than the last one, that means the current candidate
+                        // has been used
+                        // equal is needed since we can choose the same element many times   
+                        if(candidates[j] >= prevlist.get(prevlist.size()-1)){
+                            List temp = new ArrayList(prevlist); // temp is needed since 
+                            temp.add(candidates[j]); // cannot edit prevlist inside 4eeach looop
+                            combList.add(temp);
+                        }
+                    }
+                }
+            }
+            dp[t] = combList;
+        }
+        return dp[target];
+    }
+}
