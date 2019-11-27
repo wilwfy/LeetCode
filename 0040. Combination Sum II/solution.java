bc8f8dcd72e6bc21a51e6fee2f83116b277161e1
wilwfy <46659986+wilwfy@users.noreply.github.com>
2019-11-27 04:21:04 -0800

    Add other's improved solution of Backtracking

--- 0040. Combination Sum II/solution.java
+++ 0040. Combination Sum II/solution.java
@@ -30,3 +30,40 @@ class Solution {
         }
     }
 }
+
+
+/**
+ * Other's improved solution of Backtracking with line of 'break'
+ */
+class Solution {
+    public List<List<Integer>> combinationSum2(int[] candidates, int target) {
+        List<List<Integer>> res = new ArrayList<>();
+        if (candidates.length == 0) return res;
+        
+        Arrays.sort(candidates);
+
+        backtrack(0, candidates, target, res, new ArrayList<Integer>());
+        return res;
+    }
+    
+    public void backtrack(int start, int[] candid, int target, List<List<Integer>> list, List<Integer> tmpList) {
+        if (target < 0)
+            return;
+        else if (target == 0) {
+            list.add(new ArrayList<Integer>(tmpList));
+        } else {
+            for (int i = start; i < candid.length; i++) {
+                if (i > start && candid[i] == candid[i-1]) continue;
+                
+                // The array is sorted, no need to try rest numbers if negtive value got here
+                if (target-candid[i] < 0) break;
+                
+                tmpList.add(candid[i]);
+                
+                backtrack(i+1, candid, target-candid[i], list, tmpList);
+                
+                tmpList.remove(tmpList.size()-1);
+            }
+        }
+    }
+}
