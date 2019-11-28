0e9b82e9c92ab362c78f51e8851376ba3e405031
wilwfy <46659986+wilwfy@users.noreply.github.com>
2019-11-27 18:26:10 -0800

    Add solutions of Dynamic Programming

--- 0377. Combination Sum IV/solution.java
+++ 0377. Combination Sum IV/solution.java
@@ -53,3 +53,66 @@ class Solution {
         return count;
     }
 }
+
+
+/**
+ * Solution of Dynamic Programming - Top Down
+ */
+class Solution {
+    private int[] dp;
+    public int combinationSum4(int[] nums, int target) {
+        if (nums.length == 0) return 0;
+
+        dp = new int[target+1];
+        // 0 is not a good choice for initialization because it means
+        // there is no combination sum for the target.
+        Arrays.fill(dp, -1);
+        
+        // when target == 0, we think we find a combination which is one solution
+        dp[0] = 1;
+        
+        helper(nums, target);
+        return dp[target];
+    }
+    
+    public int helper(int[] nums, int target) {
+        if (dp[target] != -1)
+            return dp[target];
+        
+        int count = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (target - nums[i] < 0) continue;
+                
+            count += helper(nums, target - nums[i]);
+        }
+        
+        dp[target] = count;
+        return count;
+    }
+}
+
+
+/**
+ * Solution of Dynamic Programming - Bottom Up
+ */
+class Solution {
+
+    public int combinationSum4(int[] nums, int target) {
+        if (nums.length == 0) return 0;
+
+        int[] dp = new int[target+1];
+        
+        // when target == 0, we think we find a combination which is one solution
+        dp[0] = 1;
+        
+        for (int t = 1; t <= target; t++) {
+            for (int i = 0; i < nums.length; i++) {
+                if (t - nums[i] < 0) continue;
+                
+                dp[t] += dp[t - nums[i]];
+            }
+        }
+        
+        return dp[target];
+    }
+}
