d97e02e5a5a9d7cecd4aad12290e6380ff15c008
wilwfy <46659986+wilwfy@users.noreply.github.com>
2019-11-27 17:17:19 -0800

    Add solution of Backtracking but Time Limit Exceeded

---
+++ 0377. Combination Sum IV/solution.java
@@ -0,0 +1,31 @@
+/**
+ * Solution of Backtracking, but Time Limit Exceeded
+ */
+class Solution {
+    public int combinationSum4(int[] nums, int target) {
+        if (nums.length == 0) return 0;
+        Arrays.sort(nums);
+        if (nums[0] > target) return 0;
+        
+        //int res = 0;
+        //res = backtrack(nums, target);
+        //return res;
+        return backtrack(nums, target);
+    }
+    
+    public int backtrack(int[] nums, int target) {
+        int count = 0;
+        if (target < 0)
+            return 0;
+        else if (target == 0) {
+            return 1;
+        } else {
+            for (int i = 0; i < nums.length; i++) {
+                if (target - nums[i] < 0) break;
+                
+                count += backtrack(nums, target - nums[i]);
+            }
+            return count;
+        }
+    }
+}
