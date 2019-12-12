97d3febb53f472c77983b20b34e0120905d499a6
wilwfy <46659986+wilwfy@users.noreply.github.com>
2019-12-11 20:00:42 -0800

    Add other's solution of DP + DFS(backtracking)

--- 0131. Palindrome Partitioning/solution.java
+++ 0131. Palindrome Partitioning/solution.java
@@ -35,3 +35,40 @@ class Solution {
         return true;
     }
 }
+
+
+/**
+ * Other's solution of DP + DFS
+ * https://leetcode.com/problems/palindrome-partitioning/discuss/41982/Java-DP-%2B-DFS-solution
+ */
+class Solution {   
+    public List<List<String>> partition(String s) {
+        // generate the DP array of palindrome
+        boolean[][] dp = new boolean[s.length()][s.length()];
+        for (int i = 0; i < s.length(); i++) {
+            for (int j = 0; j <= i; j++) {
+                if (s.charAt(i) == s.charAt(j) && ((i-j <= 2) || dp[j+1][i-1]))
+                    dp[j][i] = true;
+            }
+        }
+        
+        List<List<String>> res = new ArrayList<>();
+        backtrack(0, s, dp, res, new ArrayList<>());
+        return res;
+    }
+    
+    public void backtrack(int start, String s, boolean[][] dp, List<List<String>> list, List<String> tmpList) {
+        if (start == s.length()) {
+            // the whole string has been scanned
+            list.add(new ArrayList<>(tmpList));
+        } else {
+            for (int i = start; i < s.length(); i++) {
+                if (dp[start][i]) {
+                    tmpList.add(s.substring(start, i+1));
+                    backtrack(i+1, s, dp, list, tmpList);
+                    tmpList.remove(tmpList.size()-1);
+                }
+            }
+        }
+    }
+}
