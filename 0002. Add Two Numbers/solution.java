906b106ccf644aecbee080792e475cc343b3166f
wilwfy <46659986+wilwfy@users.noreply.github.com>
2020-01-06 23:49:43 -0800

    Add official solution.

---
+++ 0002. Add Two Numbers/solution.java
@@ -0,0 +1,37 @@
+/*
+ * Official solution
+ *
+ * Time complexity : O(max(m,n)). Assume that m and n represents the length of l1 and l2 respectively, the algorithm
+ *                   iterates at most max(m,n) times.
+ * Space complexity : O(max(m,n)). The length of the new list is at most max(m,n)+1.
+ */
+class Solution {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode dummyHead = new ListNode(0);
+        ListNode p = l1, q = l2, curr = dummyHead;
+        int carry = 0;
+        while (p != null || q != null) {
+            int x = (p != null) ? p.val : 0;
+            int y = (q != null) ? q.val : 0;
+            int sum = carry + x + y;
+            carry = sum / 10;
+            curr.next = new ListNode(sum % 10);
+            curr = curr.next;
+            if (p != null) p = p.next;
+            if (q != null) q = q.next;
+        }
+        if (carry > 0) {
+            curr.next = new ListNode(carry);
+        }
+        return dummyHead.next;
+    }
+}
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
