cd797ff664613d970595a033ce7d21df64b67dc6
wilwfy <46659986+wilwfy@users.noreply.github.com>
2020-01-08 23:00:50 -0800

    Add recursive solution

--- 0206. Reverse Linked List/solution.java
+++ 0206. Reverse Linked List/solution.java
@@ -23,8 +23,30 @@ class Solution {
 }
 
 
-
-
+/*
+ * Recursive solution
+ *
+ * Time complexity : O(n). Assume that n is the list's length, the time complexity is O(n).
+ * Space complexity : O(n). The extra space comes from implicit stack space due to recursion. The recursion could go up to n levels deep.
+ */
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        
+        if ((head == null) ||
+            (head.next == null)) // we reach the last element
+            return head;
+        
+        // node is the last element of the original list and
+        // needs to be eventually returned as the result since
+        // it becomes the first element of the new list
+        ListNode node = reverseList(head.next);
+        
+        // head is current node
+        head.next.next = head;
+        head.next = null;
+        return node;
+    }
+}
 
 
 /**
