214a15d4ae86e942f652d996adfa0bb4b68265b0
wilwfy <46659986+wilwfy@users.noreply.github.com>
2020-01-17 16:23:06 -0800

    Add other's solutions of using two or one pointers

--- 0061. Rotate List/solution.java
+++ 0061. Rotate List/solution.java
@@ -37,6 +37,56 @@ class Solution {
 }
 
 
+/*
+ * Other's solution with using two pointers
+ */
+public static ListNode rotateRight(ListNode head, int k) {
+	if(head==null)
+        return null;
+	int size = 1; // since we are already at head node
+	ListNode fast=head;
+	ListNode slow = head;
+	
+	while(fast.next!=null){
+	    size++;
+	    fast = fast.next;
+	}
+	
+	for(int i=size-k%size;i>1;i--) // i>1 because we need to put slow.next at the start.
+		slow = slow.next;
+	
+    // No dummy variable.
+	fast.next = head;
+	head = slow.next;
+	slow.next = null;
+	
+	return head;
+}
+
+
+/*
+ * Other's solution with using only one pointer
+ */
+public ListNode rotateRight(ListNode head, int k) {
+    if(head == null || k == 0) {
+        return head;
+    }
+    ListNode p = head;
+    int len = 1;
+    while(p.next != null) {
+        p = p.next;
+        len++;
+    }
+    p.next = head;
+    k %= len;
+    for(int i = 0; i < len - k; i++) {
+        p = p.next;
+    }
+    head = p.next;
+    p.next = null;
+    return head;
+}
+
 
 /**
  * Definition for singly-linked list.
