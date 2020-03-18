235e7a7716f04dbe19cad59121c43953f33cd533
wilwfy <46659986+wilwfy@users.noreply.github.com>
2020-03-17 23:30:55 -0700

    Create solution.java

---
+++ 1290. Convert Binary Number in a Linked List to Integer/solution.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int getDecimalValue(ListNode head) {
+        int res = 0;
+        while (head != null) {
+            res *= 2;
+            res += head.val;
+            head = head.next;
+        }
+        return res;
+    }
+}
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
