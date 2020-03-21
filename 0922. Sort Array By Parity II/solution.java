80cee6fe808acf9cad41def6901ba95a1cd5c3ed
wilwfy <46659986+wilwfy@users.noreply.github.com>
2020-03-20 21:55:31 -0700

    Add official solutions of Two Pass and Read/Write Heads

--- 0922. Sort Array By Parity II/solution.java
+++ 0922. Sort Array By Parity II/solution.java
@@ -23,3 +23,55 @@ class Solution {
     }
 }
 
+
+/*
+ * Official solution of Two Pass
+ * 
+ * Time Complexity: O(N), where N is the length of A.
+ * Space Complexity: O(N).
+ */
+class Solution {
+    public int[] sortArrayByParityII(int[] A) {
+        int N = A.length;
+        int[] ans = new int[N];
+
+        int t = 0;
+        for (int x: A) if (x % 2 == 0) {
+            ans[t] = x;
+            t += 2;
+        }
+
+        t = 1;
+        for (int x: A) if (x % 2 == 1) {
+            ans[t] = x;
+            t += 2;
+        }
+
+        return ans;
+    }
+}
+
+
+/*
+ * Official solution of Read/Write Heads
+ * 
+ * Time Complexity: O(N), where N is the length of A.
+ * Space Complexity: O(1).
+ */
+class Solution {
+    public int[] sortArrayByParityII(int[] A) {
+        int j = 1;
+        for (int i = 0; i < A.length; i += 2)
+            if (A[i] % 2 == 1) {
+                while (A[j] % 2 == 1) {
+                    j += 2;
+                }
+                // Swap A[i] and A[j]
+                int tmp = A[i];
+                A[i] = A[j];
+                A[j] = tmp;
+            }
+
+        return A;
+    }
+}
