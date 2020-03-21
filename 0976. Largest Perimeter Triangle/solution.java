9a8c841afd8bc855210081b4b8d893df9bbc6ac2
wilwfy <46659986+wilwfy@users.noreply.github.com>
2020-03-21 04:18:41 -0700

    Create solution.java

---
+++ 0976. Largest Perimeter Triangle/solution.java
@@ -0,0 +1,28 @@
+/*
+ * My solution with higher Space Complexity
+ */
+class Solution {
+    public int largestPerimeter(int[] A) {
+        if (A.length == 3) {
+            if (isTriangle(A))
+                return A[0] + A[1] + A[2];
+            else
+                return 0;
+        } else {
+            Arrays.sort(A);
+            //System.out.println(Arrays.toString(A));
+            for (int i = A.length - 3; i >= 0; i--) {
+                int[] a = Arrays.copyOfRange(A, i, i+3);
+                if (isTriangle(a))
+                    return a[0] + a[1] + a[2];
+            }
+            return 0;
+        }
+
+    }
+    
+    public boolean isTriangle(int[] T) {
+        if (T.length != 3) return false;
+        return (T[0] + T[1] > T[2]) && (T[0] + T[2] > T[1]) && (T[1] + T[2] > T[0]);
+    }
+}
