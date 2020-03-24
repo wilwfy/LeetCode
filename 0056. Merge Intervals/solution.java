5b6e09ae1294762efb8485576d752f6a605e464e
wilwfy <46659986+wilwfy@users.noreply.github.com>
2020-03-23 17:08:18 -0700

    Add other's solution with different Comparators

--- 0056. Merge Intervals/solution.java
+++ 0056. Merge Intervals/solution.java
@@ -34,3 +34,74 @@ class Solution {
     return merged.toArray(new int[merged.size()][]);
   }
 }
+
+
+/*
+ * Other solution with different Comparator Implementation
+ */
+class Solution {
+	public int[][] merge(int[][] intervals) {
+		if (intervals.length <= 1)
+			return intervals;
+
+		// Sort by ascending starting point
+		Arrays.sort(intervals, (i1, i2) -> Integer.compare(i1[0], i2[0]));
+
+		List<int[]> result = new ArrayList<>();
+		int[] newInterval = intervals[0];
+		result.add(newInterval);
+		for (int[] interval : intervals) {
+			if (interval[0] <= newInterval[1]) // Overlapping intervals, move the end if needed
+				newInterval[1] = Math.max(newInterval[1], interval[1]);
+			else {                             // Disjoint intervals, add the new interval to the list
+				newInterval = interval;
+				result.add(newInterval);
+			}
+		}
+
+		return result.toArray(new int[result.size()][]);
+	}
+}
+
+/*
+ * Other solution with different Comparator Implementation
+ */
+public class Solution {
+    public List<Interval> merge(List<Interval> intervals) {
+        Collections.sort(intervals, new Comparator<Interval>(){
+            @Override
+            public int compare(Interval obj0, Interval obj1) {
+                return obj0.start - obj1.start;
+            }
+        });
+
+        List<Interval> ret = new ArrayList<>();
+        Interval prev = null;
+        for (Interval inter : intervals) {
+            if (  prev==null || inter.start>prev.end ) {
+                ret.add(inter);
+                prev = inter;
+            } else if (inter.end>prev.end) {
+                // Modify the element already in list
+                prev.end = inter.end;
+            }
+        }
+        return ret;
+    }
+}
+
+
+/*
+ * Other solution with different Comparator Implementation
+ */
+public class Solution {
+    public List<Interval> merge(List<Interval> intervals) {
+        intervals.sort(Comparator.comparingInt(o -> o.start));
+        LinkedList<Interval> merged = new LinkedList<>();
+        for (Interval interval : intervals)
+            if (merged.isEmpty() || merged.getLast().end < interval.start)
+                merged.add(interval);
+            else merged.getLast().end = Math.max(merged.getLast().end, interval.end);
+        return merged;
+    }
+}
