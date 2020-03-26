97f4f463207804de3fd16c428d8c622e6394b289
wilwfy <46659986+wilwfy@users.noreply.github.com>
2020-03-26 14:34:20 -0700

    Add solution without extra space

--- 0086. Partition List/solution.java
+++ 0086. Partition List/solution.java
@@ -1,5 +1,5 @@
 /*
- * My solution wit extra space.
+ * My solution with extra space.
  *
  * Time: O(n)
  * Space: O(n)
@@ -27,6 +27,35 @@ class Solution {
 }
 
 
+/*
+ * My solution without extra space.
+ *
+ * Time Complexity: O(N), where N is the number of nodes in the original linked list and we iterate the original list.
+ * Space Complexity: O(1), we have not utilized any extra space, the point to note is that we are reforming the original list,
+ *                   by moving the original nodes, we have not used any extra space as such.
+ */
+class Solution {
+    public ListNode partition(ListNode head, int x) {
+        ListNode smallDummy = new ListNode(0);
+        ListNode largeDummy = new ListNode(0);
+        ListNode small = smallDummy, large = largeDummy;
+        
+        while (head != null) {
+            if (head.val < x) {
+                small.next = head;
+                small = small.next;
+            } else {
+                large.next = head;
+                large = large.next;
+            }
+            head = head.next;
+        }
+        
+        small.next = largeDummy.next;
+        large.next = null;
+        return smallDummy.next;
+    }
+}
 /**
  * Definition for singly-linked list.
  * public class ListNode {
