34ce37c754cc93207bf54bd396068706d4ede4da
wilwfy <46659986+wilwfy@users.noreply.github.com>
2020-03-26 22:59:45 -0700

    Add official solution of Two Pointers

--- 0328. Odd Even Linked List/solution.java
+++ 0328. Odd Even Linked List/solution.java
@@ -29,6 +29,27 @@ class Solution {
 }
 
 
+/*
+ * Official concise solution wit Two Pointers
+ *
+ * Time: O(n)
+ * Space: O(1)
+ */
+class Solution {
+    public ListNode oddEvenList(ListNode head) {
+        if (head == null) return null;
+        ListNode odd = head, even = head.next, evenHead = even;
+        while (even != null && even.next != null) {
+            odd.next = even.next;
+            odd = odd.next;
+            even.next = odd.next;
+            even = even.next;
+        }
+        odd.next = evenHead;
+        return head;
+    }
+}
+
 /**
  * Definition for singly-linked list.
  * public class ListNode {
