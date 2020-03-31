24810769f4f107ef60e285a4d590a469ef1d71cd
wilwfy <46659986+wilwfy@users.noreply.github.com>
2020-03-30 23:13:57 -0700

    Add other's solution of HashMap with One Pass

--- 0697. Degree of an Array/solution.java
+++ 0697. Degree of an Array/solution.java
@@ -68,3 +68,27 @@ class Solution {
         return ans;
     }
 }
+
+
+/*
+ * Other's solution of HashMap with One Pass
+ *
+ * Time Complexity: O(N), hardly find the any reason to scan twice.
+ * Space Complexity: O(M), where M is the size of different numbers.
+ */
+class Solution {
+    public int findShortestSubArray(int[] nums) {
+        Map<Integer, Integer> count = new HashMap<>(), first = new HashMap<>();
+        int res = 0, degree = 0;
+        for (int i = 0; i < nums.length; ++i) {
+            first.putIfAbsent(nums[i], i);
+            count.put(nums[i], count.getOrDefault(nums[i], 0) + 1);
+            if (count.get(nums[i]) > degree) {
+                degree = count.get(nums[i]);
+                res = i - first.get(nums[i]) + 1;
+            } else if (count.get(nums[i]) == degree)
+                res = Math.min(res, i - first.get(nums[i]) + 1);
+        }
+        return res;
+    }
+}
