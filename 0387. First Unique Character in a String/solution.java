7f1f27c802671151c3a622e3c99913f2c98e7cb1
wilwfy <46659986+wilwfy@users.noreply.github.com>
2020-04-01 01:02:26 -0700

    Add other solutions of Two Pass with Array

--- 0387. First Unique Character in a String/solution.java
+++ 0387. First Unique Character in a String/solution.java
@@ -26,3 +26,45 @@ class Solution {
         return minIndex < Integer.MAX_VALUE ? minIndex : -1;
     }
 }
+
+
+/*
+ * Official solution of Two Pass with HashMap
+ *
+ * Time & Space: O(n)
+ */
+class Solution {
+    public int firstUniqChar(String s) {
+        if (s == null) return -1;
+        if (s.length() == 1) return 0;
+        
+        Map<Character, Integer> map = new HashMap<>();
+        for (char c: s.toCharArray())
+            map.put(c, map.getOrDefault(c, 0) + 1);
+
+        // find the index
+        for (char c: s.toCharArray()) {
+            if (map.get(c) == 1)
+                return s.indexOf(c);
+        }
+        return -1;
+    }
+}
+
+
+/*
+ * Other's solution of Two Pass with Array
+ *
+ * Time & Space: O(n)
+ */
+public class Solution {
+    public int firstUniqChar(String s) {
+        int freq [] = new int[26];
+        for(int i = 0; i < s.length(); i ++)
+            freq [s.charAt(i) - 'a'] ++;
+        for(int i = 0; i < s.length(); i ++)
+            if(freq [s.charAt(i) - 'a'] == 1)
+                return i;
+        return -1;
+    }
+}
