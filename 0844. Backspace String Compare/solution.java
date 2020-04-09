64dba6d8815e8e15c6d5e9eb33576c11a1edf27b
wilwfy <46659986+wilwfy@users.noreply.github.com>
2020-04-09 01:11:24 -0700

    Add official and other's solutions with Two Pointer

--- 0844. Backspace String Compare/solution.java
+++ 0844. Backspace String Compare/solution.java
@@ -30,3 +30,95 @@ class Solution {
             return true;
     }
 }
+
+
+/*
+ * Official solution with Stack
+ *
+ * Time Complexity: O(M+N), where M, N are the lengths of S and T respectively.
+ * Space Complexity: O(M+N).
+ */
+class Solution {
+    public boolean backspaceCompare(String S, String T) {
+        return build(S).equals(build(T));
+    }
+
+    public String build(String S) {
+        Stack<Character> ans = new Stack();
+        for (char c: S.toCharArray()) {
+            if (c != '#')
+                ans.push(c);
+            else if (!ans.empty())
+                ans.pop();
+        }
+        return String.valueOf(ans);
+    }
+}
+
+
+/*
+ * Official solution with Two Pointer
+ *
+ * Intuition:
+ * When writing a character, it may or may not be part of the final string depending on how many backspace keystrokes occur in
+ * the future.
+ * If instead we iterate through the string in reverse, then we will know how many backspace characters we have seen, and therefore
+ * whether the result includes our character.
+ *
+ * Algorithm:
+ * Iterate through the string in reverse. If we see a backspace character, the next non-backspace character is skipped. If a character
+ * isn't skipped, it is part of the final answer.
+ *
+ * Time Complexity: O(M+N), where M, N are the lengths of S and T respectively.
+ * Space Complexity: O(1).
+ */
+class Solution {
+    public boolean backspaceCompare(String S, String T) {
+        int i = S.length() - 1, j = T.length() - 1;
+        int skipS = 0, skipT = 0;
+
+        while (i >= 0 || j >= 0) { // While there may be chars in build(S) or build (T)
+            while (i >= 0) { // Find position of next possible char in build(S)
+                if (S.charAt(i) == '#') {skipS++; i--;}
+                else if (skipS > 0) {skipS--; i--;}
+                else break;
+            }
+            while (j >= 0) { // Find position of next possible char in build(T)
+                if (T.charAt(j) == '#') {skipT++; j--;}
+                else if (skipT > 0) {skipT--; j--;}
+                else break;
+            }
+            // If two actual characters are different
+            if (i >= 0 && j >= 0 && S.charAt(i) != T.charAt(j))
+                return false;
+            // If expecting to compare char vs nothing
+            if ((i >= 0) != (j >= 0))
+                return false;
+            i--; j--;
+        }
+        return true;
+    }
+}
+
+
+/*
+ * Other's solution with Two Pointer
+ *
+ * Time Complexity: O(M+N), where M, N are the lengths of S and T respectively.
+ * Space Complexity: O(1).
+ */
+class Solution {
+    public boolean backspaceCompare(String S, String T) {
+        int i = S.length() - 1, j = T.length() - 1;
+        while (true) {
+            for (int back = 0; i >= 0 && (back > 0 || S.charAt(i) == '#'); --i)
+                back += S.charAt(i) == '#' ? 1 : -1;
+            for (int back = 0; j >= 0 && (back > 0 || T.charAt(j) == '#'); --j)
+                back += T.charAt(j) == '#' ? 1 : -1;
+            if (i >= 0 && j >= 0 && S.charAt(i) == T.charAt(j)) {
+                i--; j--;
+            } else
+                return i == -1 && j == -1;
+        }
+    }
+}
