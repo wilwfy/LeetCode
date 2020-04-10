6dc39a53a1ea327075a270c3bde68f4a69d731c5
wilwfy <46659986+wilwfy@users.noreply.github.com>
2020-04-10 01:56:27 -0700

    Add other's solutions with Linked List, Two Stack and One Stack

--- 0155. Min Stack/solution.java
+++ 0155. Min Stack/solution.java
@@ -28,6 +28,167 @@ class MinStack {
     }
 }
 
+
+/*
+ * Other's solution with Node and Linked List
+ */
+class MinStack {
+    private Node head;
+    
+    public void push(int x) {
+        if(head == null) 
+            head = new Node(x, x);
+        else 
+            head = new Node(x, Math.min(x, head.min), head);
+    }
+
+    public void pop() {
+        head = head.next;
+    }
+
+    public int top() {
+        return head.val;
+    }
+
+    public int getMin() {
+        return head.min;
+    }
+    
+    private class Node {
+        int val;
+        int min;
+        Node next;
+        
+        private Node(int val, int min) {
+            this(val, min, null);
+        }
+        
+        private Node(int val, int min, Node next) {
+            this.val = val;
+            this.min = min;
+            this.next = next;
+        }
+    }
+}
+
+
+/*
+ * Other's solution with Two Stacks
+ */
+public class MinStack {
+
+    Stack<Integer> stack, min;
+
+    public MinStack() {
+        stack = new Stack<>();
+        min = new Stack<>();
+    }
+
+    public void push(int x) {
+        stack.push(x);
+        if (min.empty()) min.push(x);
+        else if (min.peek() >= x) min.push(x);
+    }
+
+    public void pop() {
+        int popped = stack.pop();
+        if (!min.empty() && min.peek() == popped) {
+            min.pop();
+        }
+    }
+
+    public int top() {
+        return stack.peek();
+    }
+
+    public int getMin() {
+        return min.peek();
+    }
+}
+
+
+/*
+ * Other's solution with One Stack
+ *
+ * But the space it used is equivalent to the Two Stacks solution
+ * And need be updated to handle Integer.MAX_VALUE when doing pop on empty stack
+ */
+class MinStack {
+    int min = Integer.MAX_VALUE;
+    Stack<Integer> stack = new Stack<Integer>();
+    public void push(int x) {
+        // only push the old minimum value when the current 
+        // minimum value changes after pushing the new value x
+        if(x <= min){          
+            stack.push(min);
+            min=x;
+        }
+        stack.push(x);
+    }
+
+    public void pop() {
+        // if pop operation could result in the changing of the current minimum value, 
+        // pop twice and change the current minimum value to the last minimum value.
+        if(stack.pop() == min) min=stack.pop();
+    }
+
+    public int top() {
+        return stack.peek();
+    }
+
+    public int getMin() {
+        return min;
+    }
+}
+
+
+/*
+ * Another solution with One Stack
+ *
+ * The idea is to store the gap between the min value and the current value;
+ * The problem for this solution is the cast. The author has no idea to avoid the cast. Since the possible gap between
+ * the current value and the min value could be Integer.MAX_VALUE-Integer.MIN_VALUE;
+ */
+public class MinStack {
+    long min;
+    Stack<Long> stack;
+
+    public MinStack(){
+        stack=new Stack<>();
+    }
+    
+    public void push(int x) {
+        if (stack.isEmpty()){
+            stack.push(0L);
+            min=x;
+        }else{
+            stack.push(x-min);//Could be negative if min value needs to change
+            if (x<min) min=x;
+        }
+    }
+
+    public void pop() {
+        if (stack.isEmpty()) return;
+        
+        long pop=stack.pop();
+        
+        if (pop<0)  min=min-pop;//If negative, increase the min value
+        
+    }
+
+    public int top() {
+        long top=stack.peek();
+        if (top>0){
+            return (int)(top+min);
+        }else{
+           return (int)(min);
+        }
+    }
+
+    public int getMin() {
+        return (int)min;
+    }
+}
 /**
  * Your MinStack object will be instantiated and called as such:
  * MinStack obj = new MinStack();
