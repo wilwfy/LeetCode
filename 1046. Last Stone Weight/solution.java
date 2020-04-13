b35d285b6e4795cb202670c5f6b84d3e0e288df1
wilwfy <46659986+wilwfy@users.noreply.github.com>
2020-04-13 00:17:41 -0700

    Add other's solution of bucket sort and two pointers

--- 1046. Last Stone Weight/solution.java
+++ 1046. Last Stone Weight/solution.java
@@ -29,3 +29,40 @@ class Solution {
         return pq.poll();
     }
 }
+
+
+/*
+ * Other's solution of bucket sort and two pointers
+ *
+ * Time: Average O(N + K), Worst O(N^2). N is length of stones, K is the value range of stones.
+ * Space: O(N + K)
+ */
+class Solution {
+    public int lastStoneWeight(int[] stones) {
+        int[] buckets = new int[1001];
+        for (int i = 0; i < stones.length; i++) {
+            buckets[stones[i]]++;
+        }
+
+        int slow = buckets.length - 1;   //start from the big to small
+        while (slow > 0) {
+		// If the number of stones with the same size is even or zero, 
+		// these stones can be totally destroyed pair by pair or there is no such size stone existing, 
+		// we can just ignore this situation.
+		
+        // When the number of stones with the same size is odd, 
+		// there should leave one stone which is to smash with the smaller size one.
+            if (buckets[slow]%2 != 0) {
+                int fast = slow - 1;
+                while (fast > 0 && buckets[fast] == 0) {
+                    fast--;
+                }
+                if (fast == 0) break;
+                buckets[fast]--;
+                buckets[slow - fast]++;
+            }
+            slow--;
+        }
+        return slow;
+    }
+}
