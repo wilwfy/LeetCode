c63e3ad8fd7f4551a51535ef2534c1c9f2e96640
wilwfy <46659986+wilwfy@users.noreply.github.com>
2020-04-16 12:33:00 -0700

    Add official and other's Greedy solutions with One Pass

--- 0678. Valid Parenthesis String/solution.java
+++ 0678. Valid Parenthesis String/solution.java
@@ -97,3 +97,88 @@ class Solution {
     }
 }
 
+
+/*
+ * Official solution of Greedy and One Pass
+ *
+ * Intuition
+ * When checking whether the string is valid, we only cared about the "balance": the number of extra, open left brackets as we parsed
+ * through the string. For example, when checking whether '(()())' is valid, we had a balance of 1, 2, 1, 2, 1, 0 as we parse through
+ * the string: '(' has 1 left bracket, '((' has 2, '(()' has 1, and so on. This means that after parsing the first i symbols, (which
+ * may include asterisks,) we only need to keep track of what the balance could be.
+ * For example, if we have string '(***)', then as we parse each symbol, the set of possible values for the balance is [1] for '(';
+ * [0, 1, 2] for '(*'; [0, 1, 2, 3] for '(**'; [0, 1, 2, 3, 4] for '(***', and [0, 1, 2, 3] for '(***)'.
+ * Furthermore, we can prove these states always form a contiguous interval. Thus, we only need to know the left and right bounds of
+ * this interval. That is, we would keep those intermediate states described above as [lo, hi] = [1, 1], [0, 2], [0, 3], [0, 4], [0, 3].
+ *
+ * Algorithm
+ * Let lo, hi respectively be the smallest and largest possible number of open left brackets after processing the current character in
+ * the string.
+ * If we encounter a left bracket (c == '('), then lo++, otherwise we could write a right bracket, so lo--. If we encounter what can be
+ * a left bracket (c != ')'), then hi++, otherwise we must write a right bracket, so hi--. If hi < 0, then the current prefix can't be
+ * made valid no matter what our choices are. Also, we can never have less than 0 open left brackets. At the end, we should check that
+ * we can have exactly 0 open left brackets.
+ *
+ * Time Complexity: O(N), where N is the length of the string. We iterate through the string once.
+ * Space Complexity: O(1), the space used by our lo and hi pointers. However, creating a new character array will take O(N) space.
+ */
+class Solution {
+    public boolean checkValidString(String s) {
+       int lo = 0, hi = 0;
+       for (char c: s.toCharArray()) {
+           lo += c == '(' ? 1 : -1;
+           hi += c != ')' ? 1 : -1;
+           if (hi < 0) break;
+           lo = Math.max(lo, 0);
+       }
+       return lo == 0;
+    }
+}
+
+
+/*
+ * Other's solution of Greedy and One Pass
+ *
+ * Intuition:
+ * One pass on the string S, we need to know, how many ')' we are waiting for.
+ * If we meet too many ')', we can return false directly.
+ * If we wait for no ')' at the end, then we are good.
+ *
+ * Explanation:
+ * We count the number of ')' we are waiting for, and it's equal to the number of open parenthesis.
+ * This number will be in a range and we count it as [cmin, cmax]
+ * cmax counts the maximum open parenthesis, which means the maximum number of unbalanced '(' that COULD be paired.
+ * cmin counts the minimum open parenthesis, which means the number of unbalanced '(' that MUST be paired.
+ *
+ * Example:
+ * It's quite straight forward actually.
+ * When you met "(", you know you need one only one ")", cmin = 1 and cmax = 1.
+ * When you met "(*(", you know you need one/two/three ")", cmin = 1 and cmax = 3.
+ *
+ * The string is valid for 2 condition:
+ * cmax will never be negative.
+ * cmin is 0 at the end.
+ *
+ * Time Complexity: One pass, O(N) time.
+ * Space Complexity: O(1)
+ */
+class Solution {
+    public boolean checkValidString(String s) {
+        int cmin = 0, cmax = 0;
+        for (int i = 0; i < s.length(); ++i) {
+            char c = s.charAt(i);
+            if (c == '(') {
+                cmax++;
+                cmin++;
+            } else if (c == ')') {
+                cmax--;
+                cmin = Math.max(cmin - 1, 0);
+            } else {
+                cmax++;
+                cmin = Math.max(cmin - 1, 0);
+            }
+            if (cmax < 0) return false;
+        }
+        return cmin == 0;
+    }
+}
