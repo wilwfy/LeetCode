bafd96092799cabe29e2588db6aa7892757ec1af
wilwfy <46659986+wilwfy@users.noreply.github.com>
2020-04-16 21:19:49 -0700

    Add other's solution of Depth-First Search

--- 0543. Diameter of Binary Tree/solution.java
+++ 0543. Diameter of Binary Tree/solution.java
@@ -32,6 +32,29 @@ class Solution {
 }
 
 
+/*
+ * Other's solution of Depth-First Search
+ */
+public class Solution {
+    int max = 0;
+    
+    public int diameterOfBinaryTree(TreeNode root) {
+        maxDepth(root);
+        return max;
+    }
+    
+    private int maxDepth(TreeNode root) {
+        if (root == null) return 0;
+        
+        int left = maxDepth(root.left);
+        int right = maxDepth(root.right);
+        
+        max = Math.max(max, left + right);
+        
+        return Math.max(left, right) + 1;
+    }
+}
+
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
