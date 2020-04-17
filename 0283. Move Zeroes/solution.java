36d6a2286159c966f7969069f61976cd1c948438
wilwfy <46659986+wilwfy@users.noreply.github.com>
2020-04-16 21:41:06 -0700

    Add other's solutions with in-place sorting

--- 0283. Move Zeroes/solution.java
+++ 0283. Move Zeroes/solution.java
@@ -16,3 +16,53 @@ class Solution {
         }
     }
 }
+
+
+/*
+ * Other's solution
+ *
+ * Shift non-zero values as far forward as possible, fill remaining space with zeros.
+ *
+ * Time Complexity: O(n). However, the total number of operations are still sub-optimal. The total operations (array writes) that
+ *                  code does is n (Total number of elements).
+ * Space Complexity: O(1). Only constant space is used.
+ */
+class Solution {
+    public void moveZeroes(int[] nums) {
+        if (nums == null || nums.length == 0) return;        
+    
+        int insertPos = 0;
+        for (int num: nums) {
+            if (num != 0) nums[insertPos++] = num;
+        }        
+    
+        while (insertPos < nums.length) {
+            nums[insertPos++] = 0;
+        }
+    }
+}
+
+
+/*
+ * Other's solution
+ *
+ * Set leftMostZeroIndex to 0. Iterate through the array, at each iteration i, if nums[i] != 0 and i > leftMostZeroIndex, replace
+ * the leftmost zero element nums[leftMostZeroIndex] with nums[i], and set nums[i] to 0.
+ *
+ * Time Complexity: O(n). 
+ * Space Complexity: O(1). Only constant space is used.
+ */
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int leftMostZeroIndex = 0; // The index of the leftmost zero
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != 0) {
+                if (i > leftMostZeroIndex) { // There are zero(s) on the left side of the current non-zero number, swap!
+                    nums[leftMostZeroIndex] = nums[i];
+                    nums[i] = 0;
+                }
+                leftMostZeroIndex++;
+            }
+        }
+    }
+}
