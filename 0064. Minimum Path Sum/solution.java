c97879f0a71aebb58d3769267a693040d32e5468
wilwfy <46659986+wilwfy@users.noreply.github.com>
2020-04-19 01:23:26 -0700

    Add solutions of Dynamic Programming

--- 0064. Minimum Path Sum/solution.java
+++ 0064. Minimum Path Sum/solution.java
@@ -1,6 +1,7 @@
 /**
  * My solution of recursion (TLE - Time Limit Exceeded)
  *
+ * Space: O(n * m)
  */
 class Solution {
     int n;
@@ -24,5 +25,79 @@ class Solution {
         // Otherwise, move rightwards or move downwards
         helper(i, j+1, curSum);
         helper(i+1, j, curSum);
+    }
+}
+
+
+/**
+ * My solution of Dynamic Programming
+ *
+ * Space: O(n * m)
+ */
+class Solution {
+    public int minPathSum(int[][] grid) {
+    	int n = grid.length; // row
+        if (n == 0) return 0;
+    	int m = grid[0].length; // column
+        int[][] minSum = new int[n][m];
+        minSum[0][0] = grid[0][0];
+        for (int i = 1; i < n; i++)
+            minSum[i][0] = minSum[i-1][0] + grid[i][0];
+        for (int j = 1; j < m; j++)
+            minSum[0][j] = minSum[0][j-1] + grid[0][j];
+        for (int i = 1; i < n; i++) {
+            for (int j = 1; j < m; j++) {
+                minSum[i][j] = Math.min(minSum[i-1][j], minSum[i][j-1]) + grid[i][j];
+            }
+        }
+        return minSum[n-1][m-1];
+    }
+}
+
+
+/**
+ * Other's solution of in-place Dynamic Programming
+ *
+ * Space: O(1). But the original data are modified
+ */
+class Solution {
+    public int minPathSum(int[][] grid) {
+    	int m = grid.length; // row
+    	int n = grid[0].length; // column
+    	for(int j = 1; j < n; j++) grid[0][j] = grid[0][j] + grid[0][j-1];
+    	for(int i = 1; i < m; i++) grid[i][0] = grid[i][0] + grid[i-1][0];
+    	for (int i = 1; i < m; i++) {
+    		for (int j = 1; j < n; j++) {
+    				grid[i][j] = Math.min(grid[i][j - 1], grid[i - 1][j]) + grid[i][j];
+    		}
+    	}
+    	return grid[m - 1][n - 1];
+    }
+}
+
+
+/**
+ * Other's solution of Dynamic Programming with O(n) space
+ *
+ * Space: O(n)
+ */
+class Solution {
+    public int minPathSum(int[][] grid) {
+        int m = grid.length, n = grid[0].length;
+        int[] dp = new int[n];
+        dp[0] = grid[0][0];
+        for (int j = 1; j < n; j++) {
+            dp[j] = dp[j - 1] + grid[0][j];
+        }
+        for (int i = 1; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (j == 0) {
+                    dp[j] += grid[i][j];
+                } else {
+                    dp[j] = grid[i][j] + Math.min(dp[j], dp[j - 1]);
+                }
+            }
+        }
+        return dp[n - 1];
     }
 }
