941e4b7910496d4b8ad97fd5945d60ac2a5ddd74
wilwfy <46659986+wilwfy@users.noreply.github.com>
2020-04-21 21:20:47 -0700

    Create solution.java

---
+++ 0000. Good Discussions/Other Problems/Leftmost Column with at Least a One/solution.java
@@ -0,0 +1,26 @@
+/**
+ * // This is the BinaryMatrix's API interface.
+ * // You should not implement it, or speculate about its implementation
+ * interface BinaryMatrix {
+ *     public int get(int x, int y) {}
+ *     public List<Integer> dimensions {}
+ * };
+ */
+
+class Solution {
+    public int leftMostColumnWithOne(BinaryMatrix binaryMatrix) {
+        List<Integer> dimensions = binaryMatrix.dimensions();
+        int m = dimensions.get(0), n = dimensions.get(0);
+        int leftMostCol = -1;
+        int y = n-1, x = 0;
+        while ((y >= 0) && (x < m)) {
+            if (binaryMatrix.get(x, y) == 0)
+                x++;
+            else {
+                leftMostCol = y;
+                y--;
+            }
+        }
+        return leftMostCol;
+    }
+}
