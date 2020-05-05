3bcb21b2f37e98fc832c885f1a54fe3428117da2
wilwfy <46659986+wilwfy@users.noreply.github.com>
2020-05-04 21:37:50 -0700

    Add more other's solution of bit-manipulation

--- 0476. Number Complement/solution.java
+++ 0476. Number Complement/solution.java
@@ -31,3 +31,27 @@ public class Solution {
         return n - num;
     }
 }
+
+
+/**
+ * Other's solution of bit-manipulation
+ ×
+ * input: 5 = 0...0101(+)
+ * output: 2 = 0...0010(+)
+ * input + output = 0...0111(+)
+ * ~5 = 1..1010 (-)（all bits inverse）
+ * (Integer.highestOneBit(num) << 1) - 1 //it's mean 1000-1=0..0111(+)
+ × ~5 & (Integer.highestOneBit(num) << 1) - 1  //result is  0...0 010
+ */
+public class Solution {
+    public int findComplement(int num) {
+        return ~num & (Integer.highestOneBit(num) - 1);
+    }
+}
+
+public class Solution {
+    public int findComplement(int num) {
+        int mask = (Integer.highestOneBit(num) << 1) - 1;
+        return num ^ mask;
+    }
+}
