d6fd23a45314fc302f273ab63e5cd5fe51a9c108
wilwfy <46659986+wilwfy@users.noreply.github.com>
2020-05-15 00:40:24 -0700

    Add other's solutions of Backtracking without extra space

--- 0079. Word Search/solution.java
+++ 0079. Word Search/solution.java
@@ -1,3 +1,6 @@
+/**
+ * My solution of Backtracking with extra space
+ */
 class Solution {
     public boolean exist(char[][] board, String word) {
         if ((board == null) || (board.length == 0)) return false;
@@ -26,5 +29,61 @@ class Solution {
         res = res || helper(board, visited, row + 1, col, word.substring(1));
         visited[row][col] = 0;
         return res;
+    }
+}
+
+
+/**
+ * Other's solution of Backtracking without extra space
+ */
+class Solution {
+    public boolean exist(char[][] board, String word) {
+        for(int i = 0; i < board.length; i++)
+            for(int j = 0; j < board[0].length; j++){
+                if(exist(board, i, j, word, 0))
+                    return true;
+            }
+        return false;
+    }
+    private boolean exist(char[][] board, int i, int j, String word, int ind){
+        if(ind == word.length()) return true;
+        if(i > board.length-1 || i <0 || j<0 || j >board[0].length-1 || board[i][j]!=word.charAt(ind))
+            return false;
+        board[i][j]='*';
+        boolean result =    exist(board, i-1, j, word, ind+1) ||
+                            exist(board, i, j-1, word, ind+1) ||
+                            exist(board, i, j+1, word, ind+1) ||
+                            exist(board, i+1, j, word, ind+1);
+        board[i][j] = word.charAt(ind);
+        return result;
+    }
+}
+
+
+/**
+ * Other's solution of Backtracking without extra space
+ */
+class Solution {
+    public boolean exist(char[][] board, String word) {
+        char[] w = word.toCharArray();
+        for (int y=0; y<board.length; y++) {
+        	for (int x=0; x<board[y].length; x++) {
+        		if (exist(board, y, x, w, 0)) return true;
+        	}
+        }
+        return false;
+    }
+    
+    private boolean exist(char[][] board, int y, int x, char[] word, int i) {
+    	if (i == word.length) return true;
+    	if (y<0 || x<0 || y == board.length || x == board[y].length) return false;
+    	if (board[y][x] != word[i]) return false;
+    	board[y][x] ^= 256;
+    	boolean exist = exist(board, y, x+1, word, i+1)
+    		|| exist(board, y, x-1, word, i+1)
+    		|| exist(board, y+1, x, word, i+1)
+    		|| exist(board, y-1, x, word, i+1);
+    	board[y][x] ^= 256;
+    	return exist;
     }
 }
