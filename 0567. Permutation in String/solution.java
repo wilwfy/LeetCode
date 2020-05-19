bbbd965b184ce848fb1d2c91120ba6a552089776
wilwfy <46659986+wilwfy@users.noreply.github.com>
2020-05-19 12:10:44 -0700

    Add official solutions with Array or Sliding Window

---
+++ 0567. Permutation in String/solution.java
@@ -0,0 +1,105 @@
+/**
+ * Official solution with using Array
+ *
+ * Time complexity : O(l_1 + 26 * l_1 * (l_2 - l_1)), where l_1 is the length of string l_1 and l_2 is the length of string l_2.
+ * Space complexity : O(1). s1map and s2map of size 26 is used.
+ */
+public class Solution {
+    public boolean checkInclusion(String s1, String s2) {
+        if (s1.length() > s2.length())
+            return false;
+        int[] s1map = new int[26];
+        for (int i = 0; i < s1.length(); i++)
+            s1map[s1.charAt(i) - 'a']++;
+        for (int i = 0; i <= s2.length() - s1.length(); i++) {
+            int[] s2map = new int[26];
+            for (int j = 0; j < s1.length(); j++) {
+                s2map[s2.charAt(i + j) - 'a']++;
+            }
+            if (matches(s1map, s2map))
+                return true;
+        }
+        return false;
+    }
+    public boolean matches(int[] s1map, int[] s2map) {
+        for (int i = 0; i < 26; i++) {
+            if (s1map[i] != s2map[i])
+                return false;
+        }
+        return true;
+    }
+}
+
+
+/**
+ * Official solution with Sliding Window
+ *
+ * Time complexity : O(l_1 + 26 * (l_2 - l_1)), where l_1 is the length of string l_1 and l_2 is the length of string l_2.
+ * Space complexity : O(1). Constant space is used.
+ */
+public class Solution {
+    public boolean checkInclusion(String s1, String s2) {
+        if (s1.length() > s2.length())
+            return false;
+        int[] s1map = new int[26];
+        int[] s2map = new int[26];
+        for (int i = 0; i < s1.length(); i++) {
+            s1map[s1.charAt(i) - 'a']++;
+            s2map[s2.charAt(i) - 'a']++;
+        }
+        for (int i = 0; i < s2.length() - s1.length(); i++) {
+            if (matches(s1map, s2map))
+                return true;
+            s2map[s2.charAt(i + s1.length()) - 'a']++;
+            s2map[s2.charAt(i) - 'a']--;
+        }
+        return matches(s1map, s2map);
+    }
+    public boolean matches(int[] s1map, int[] s2map) {
+        for (int i = 0; i < 26; i++) {
+            if (s1map[i] != s2map[i])
+                return false;
+        }
+        return true;
+    }
+}
+
+
+/**
+ * Official solution with Optimized Sliding Window
+ *
+ * Time complexity : O(l_1 + (l_2 - l_1)), where l_1 is the length of string l_1 and l_2 is the length of string l_2.
+ * Space complexity : O(1). Constant space is used.
+ */
+public class Solution {
+    public boolean checkInclusion(String s1, String s2) {
+        if (s1.length() > s2.length())
+            return false;
+        int[] s1map = new int[26];
+        int[] s2map = new int[26];
+        for (int i = 0; i < s1.length(); i++) {
+            s1map[s1.charAt(i) - 'a']++;
+            s2map[s2.charAt(i) - 'a']++;
+        }
+        int count = 0;
+        for (int i = 0; i < 26; i++)
+            if (s1map[i] == s2map[i])
+                count++;
+        for (int i = 0; i < s2.length() - s1.length(); i++) {
+            int r = s2.charAt(i + s1.length()) - 'a', l = s2.charAt(i) - 'a';
+            if (count == 26)
+                return true;
+            s2map[r]++;
+            if (s2map[r] == s1map[r])
+                count++;
+            else if (s2map[r] == s1map[r] + 1)
+                count--;
+            s2map[l]--;
+            if (s2map[l] == s1map[l])
+                count++;
+            else if (s2map[l] == s1map[l] - 1)
+                count--;
+        }
+        return count == 26;
+    }
+}
