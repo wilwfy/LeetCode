dc25c0862dd048a875741f554e93c828b738e4f8
wilwfy <46659986+wilwfy@users.noreply.github.com>
2020-05-27 00:05:34 -0700

    Add solutions of DP with less space (1D)

--- 1035. Uncrossed Lines/solution.java
+++ 1035. Uncrossed Lines/solution.java
@@ -18,3 +18,73 @@ class Solution {
         return dp[A.length][B.length];
     }
 }
+
+
+/**
+ * My solution of DP with less space
+ *
+ * Time Complexity: O(m * n)
+ * Space Complexity: O(m)
+ */
+class Solution {
+    public int maxUncrossedLines(int[] A, int[] B) {
+        int[] dp = new int[B.length+1];
+        int prev = 0;
+        for (int i = 1; i <= A.length;  i++) {
+            prev = 0;
+            for (int j = 1; j <= B.length; j++) {
+                int cur = dp[j];
+                if (A[i-1] == B[j-1])
+                    dp[j] = prev + 1;
+                else
+                    dp[j] = Math.max(dp[j-1], dp[j]);
+                
+                prev = cur;
+            }
+        }
+        return dp[B.length];
+    }
+}
+
+
+/**
+ * Other's solution of DP
+ *
+ * Time O(N^2), Space O(N^2)
+ */
+class Solution {
+    public int maxUncrossedLines(int[] A, int[] B) {
+        int m = A.length, n = B.length, dp[][] = new int[m + 1][n + 1];
+        for (int i = 1; i <= m; ++i)
+            for (int j = 1; j <= n; ++j)
+                if (A[i - 1] == B[j - 1])
+                    dp[i][j] = 1 + dp[i - 1][j - 1];
+                else
+                    dp[i][j] = Math.max(dp[i][j - 1], dp[i - 1][j]);
+        return dp[m][n];
+    }
+}
+
+
+/**
+ * Other's solution of DP
+ *
+ * Time O(N^2), Space O(N)
+ */
+class Solution {
+    public int maxUncrossedLines(int[] A, int[] B) {
+        int m = A.length, n = B.length;
+        if (m < n) return maxUncrossedLines(B, A);
+	    
+        int dp[] = new int[n+1];
+        for (int i = 0; i < m; i++) {
+            for (int j = 0, prev = 0, cur; j < n; j++) {
+                cur = dp[j+1];
+                if (A[i] == B[j]) dp[j+1] = 1+prev;
+                else dp[j+1] = Math.max(dp[j+1], dp[j]);
+                prev = cur;
+            }
+        }
+        return dp[n];
+    }
+}
