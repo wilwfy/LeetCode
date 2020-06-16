d30ad4c01ff4fe25829ba1ebf73b56942d6555d5
wilwfy <46659986+wilwfy@users.noreply.github.com>
2020-06-16 00:27:51 -0700

    Add other's solution of Recursion

--- 0865. Smallest Subtree with all the Deepest Nodes/solution.java
+++ 0865. Smallest Subtree with all the Deepest Nodes/solution.java
@@ -1,4 +1,44 @@
 /**
+ * Other's solution of Recursion
+ *
+ * Intuitively, we should be traversing from the children to the parent and calculate the
+ * height from bottom. So the null nodes would have height 0. The leaf nodes would have the
+ * height 1 and the root would have the max height.
+ 
+ * At each node, we keep a pair<height_of_node_from_bottom, node>. At a given node, if we
+ * realize that the leftHeight == rightHeight, it means we have found the deepest subtree
+ * rooted at node. If leftHeight > rightHeight, it means the deepest subtree must be rooted
+ * at left child. If rightHeight > leftHeight, it means the deepest subtree must be rooted
+ * at right child.
+ *
+ * Which traversal allows us to traverse from bottom-up? Postorder! So we use it in the code.
+ *
+ * Time: O(N)
+ * Space: O(height)
+ */
+class Solution {
+    public TreeNode subtreeWithAllDeepest(TreeNode root) {
+        return height(root).getValue();
+    }
+
+    private Pair<Integer, TreeNode> height(TreeNode root) {
+        if (root == null) return new Pair(0, null);
+        
+        Pair<Integer, TreeNode> left = height(root.left);
+        Pair<Integer, TreeNode> right = height(root.right);
+        
+        int leftHeight = left.getKey(), rightHeight = right.getKey();
+        if (leftHeight == rightHeight)
+            return new Pair(leftHeight+1, root);
+        else if (leftHeight > rightHeight)
+            return new Pair(leftHeight+1, left.getValue());
+        else
+            return new Pair(rightHeight+1, right.getValue());
+    }
+}
+
+
+/**
  * Official solution of Recursion
  *
  * Intuition
