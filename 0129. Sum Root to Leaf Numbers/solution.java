097357a19a8f05fc55deed86d3d69b973d48d8c3
wilwfy <46659986+wilwfy@users.noreply.github.com>
2020-06-26 21:12:20 -0700

    Add other's solution of DFS

--- 0129. Sum Root to Leaf Numbers/solution.java
+++ 0129. Sum Root to Leaf Numbers/solution.java
@@ -29,6 +29,23 @@ class Solution {
 
 
 /**
+ * Other's solution of DFS
+ *
+ * Time: O(H). H is the height of the tree
+ * Sapce: O(1)
+ */
+class Solution {
+    public int sumNumbers(TreeNode root) {
+    	return sum(root, 0);
+    }
+    
+    public int sum(TreeNode n, int s){
+    	if (n == null) return 0;
+    	if (n.right == null && n.left == null) return s*10 + n.val;
+    	return sum(n.left, s*10 + n.val) + sum(n.right, s*10 + n.val);
+    }
+}
+/**
  * Definition for a binary tree node.
  * public class TreeNode {
  *     int val;
