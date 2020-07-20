176d910f23cafd8848e3d6f82d65dd9fdf1776f3
wilwfy <46659986+wilwfy@users.noreply.github.com>
2020-07-20 15:48:25 -0700

    Add other's solution with carry bit

---
+++ 0067. Add Binary/solution.java
@@ -0,0 +1,18 @@
+/**
+ * Other's solution with carry bit
+ */
+class Solution {
+    public String addBinary(String a, String b) {
+        StringBuilder res = new StringBuilder();
+        int i = a.length() - 1, j = b.length() - 1, carry = 0;
+        while (i >= 0 || j >= 0) {
+            int sum = carry;
+            if (j >= 0) sum += b.charAt(j--) - '0';
+            if (i >= 0) sum += a.charAt(i--) - '0';
+            res.append(sum % 2);
+            carry = sum / 2;
+        }
+        if (carry != 0) res.append(carry);
+        return res.reverse().toString();
+    }
+}
