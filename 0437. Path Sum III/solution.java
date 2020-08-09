27c5a0e78a664d05230b9d936d26d892a430d1be
wilwfy <46659986+wilwfy@users.noreply.github.com>
2020-08-08 23:49:29 -0700

    Add other's DFS solution with HashMap of Prefix Sum

--- 0437. Path Sum III/solution.java
+++ 0437. Path Sum III/solution.java
@@ -19,6 +19,38 @@ class Solution {
 }
 
 
+/**
+ * Other's solution of DFS with HashMap of Prefix Sum
+ *
+ * Time: O(n). Need walk through every node in the tree.
+ * Space: O(h) due to recursion and h is the height of the tree.
+ */
+class Solution {
+    public int pathSum(TreeNode root, int sum) {
+        if (root == null) return 0;
+        // The map stores <prefix sum, frequency> pairs before getting to the current node.
+        Map<Integer, Integer> preSumMap = new HashMap<>();
+        preSumMap.put(0, 1);
+        return helper(root, 0, sum, preSumMap);
+    }
+    
+    private int helper(TreeNode node, int currSum, int target, Map<Integer, Integer> preSumMap) {
+        if (node == null) return 0;
+        // update the prefix sum by adding the current val
+        currSum += node.val;
+        // get the number of valid path, ended by the current node.
+        // Need do this prior to updating the map since the target could be 0 which leads to currSum - target = currSum
+        int numPathToCurr = preSumMap.getOrDefault(currSum - target, 0);
+        // update the map with the current sum, so the map is good to be passed to the next recursion
+        preSumMap.put(currSum, preSumMap.getOrDefault(currSum, 0) + 1);
+        
+        // add the 3 parts together
+        int res = numPathToCurr + helper(node.left, currSum, target, preSumMap) + helper(node.right, currSum, target, preSumMap);
+        // restore the map, as the recursion goes from the bottom to the top
+        preSumMap.put(currSum, preSumMap.get(currSum) - 1);
+        return res;
+    }
+}
 
 /**
  * Definition for a binary tree node.
