5d20c3fca92c5d4d1980714315a24465ba0ab636
wilwfy <46659986+wilwfy@users.noreply.github.com>
2020-08-11 23:49:37 -0700

    Add my solution of Recursion

---
+++ 1379. Find a Corresponding Node of a Binary Tree in a Clone of That Tree/solution.java
@@ -0,0 +1,24 @@
+/**
+ * My solution of Recursion
+ *
+ * Time: O(n)
+ * Space: O(n). Recursion space. Worst case that all the nodes in the tree has only left subtree or only right subtree.
+ */
+class Solution {
+    public final TreeNode getTargetCopy(final TreeNode original, final TreeNode cloned, final TreeNode target) {
+        if (original == null) return null;
+        if (original.val == target.val) return cloned;
+        TreeNode node = getTargetCopy(original.left, cloned.left, target);
+        return (node != null) ? node : getTargetCopy(original.right, cloned.right, target);
+    }
+}
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
