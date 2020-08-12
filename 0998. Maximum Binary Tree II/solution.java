5e780170048cf17913d94265c64b875414521624
wilwfy <46659986+wilwfy@users.noreply.github.com>
2020-08-11 23:20:12 -0700

    Add other's solutions of Recursion and Iteration

--- 0998. Maximum Binary Tree II/solution.java
+++ 0998. Maximum Binary Tree II/solution.java
@@ -32,6 +32,56 @@ class Solution {
 
 
 /**
+ * Other's solution of Recursion
+ *
+ * If root.val > val, recusion on the right.
+ * Else, put right subtree on the left of new node TreeNode(val)
+ *
+ * Time: O(n)
+ * Space: O(n). Recursion space.
+ */
+class Solution {
+    public TreeNode insertIntoMaxTree(TreeNode root, int val) {
+        if (root != null && root.val > val) {
+            root.right = insertIntoMaxTree(root.right, val);
+            return root;
+        }
+        TreeNode node = new TreeNode(val);
+        node.left = root;
+        return node;
+    }
+}
+
+
+/**
+ * Other's solution of Iteration
+ *
+ * Search on the right, find the node that cur.val > val > cur.right.val
+ * Then create new node TreeNode(val),
+ * put old cur.right as node.left,
+ * put node as new cur.right.
+ *
+ * Time: O(n)
+ * Space: O(1)
+ */
+class Solution {
+    public TreeNode insertIntoMaxTree(TreeNode root, int val) {
+        TreeNode node = new TreeNode(val), cur = root;
+        if (root == null) return node;
+        if (root.val < val) {
+            node.left = root;
+            return node;
+        }
+        
+        while (cur.right != null && cur.right.val > node.val)
+            cur = cur.right;
+        
+        node.left = cur.right;
+        cur.right = node;
+        return root;
+    }
+}
+/**
  * Definition for a binary tree node.
  * public class TreeNode {
  *     int val;
