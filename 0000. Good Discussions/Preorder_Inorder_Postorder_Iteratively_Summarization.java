fee8ee6a6e292305a675ca6b9cc6707bc073186b
wilwfy <46659986+wilwfy@users.noreply.github.com>
2020-08-13 16:46:11 -0700

    Update Preorder_Inorder_Postorder_Iteratively_Summarization.java

--- 0000. Good Discussions/Preorder_Inorder_Postorder_Iteratively_Summarization.java
+++ 0000. Good Discussions/Preorder_Inorder_Postorder_Iteratively_Summarization.java
@@ -56,3 +56,67 @@ public List<Integer> postorderTraversal(TreeNode root) {
     }
     return result;
 }
+
+//////////////////////////////////////////////////////////////////////////////////
+
+// Writing traversal using stack:
+// inorder
+/*---------------------------------------------------------------*/
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> res=new ArrayList<>();
+        if (root==null) return res;
+
+        Stack<TreeNode> stack=new Stack<>();
+        TreeNode curr=root;
+
+        while(curr!=null || !stack.isEmpty()){
+            while (curr!=null){
+                stack.push(curr);
+                curr=curr.left;
+            }
+            curr=stack.pop();
+            res.add(curr.val);
+            curr=curr.right;
+        }
+        return res;
+    }
+
+// preorder
+/*---------------------------------------------------------------*/
+    public List<Integer> preorderTraversal(TreeNode root) {
+        List<Integer> list = new ArrayList<>();
+        if(root == null) return list;
+        Stack<TreeNode> stack = new Stack<>();
+        stack.push(root);
+        while(!stack.isEmpty()) {
+            TreeNode current = stack.pop();
+            list.add(current.val);
+            if(current.right!=null) {
+               stack.push(current.right);
+            }
+            if(current.left!=null) {
+              stack.push(current.left);
+            }
+        }
+        return list;
+    }
+
+// postorder
+/*---------------------------------------------------------------*/
+    public List<Integer> postorderTraversal(TreeNode root) {
+        List<Integer> list = new ArrayList<>();
+        if(root == null) return list;
+        Stack<TreeNode> stack = new Stack<>();
+        stack.push(root);
+        while(!stack.isEmpty()) {
+            TreeNode curr = stack.pop();
+            list.add(0,curr.val);
+            if(curr.left!=null) {
+              stack.push(curr.left);
+            }
+            if(curr.right!=null) {
+               stack.push(curr.right); 
+            }
+        }
+        return list;
+    }
