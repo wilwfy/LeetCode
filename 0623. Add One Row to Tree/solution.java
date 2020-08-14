5ed5f3092361daa7d0f1ef777ebacea42160a34f
wilwfy <46659986+wilwfy@users.noreply.github.com>
2020-08-14 11:17:20 -0700

    Add other's solutions of BFS and DFS

--- 0623. Add One Row to Tree/solution.java
+++ 0623. Add One Row to Tree/solution.java
@@ -1,7 +1,7 @@
 /**
  * My Iteration solution
  *
- * Time: O(2^d)
+ * Time: O(2^d). But O(n) in worst case.
  * Space: O(2^d)
  */
 class Solution {
@@ -35,6 +35,99 @@ class Solution {
             if (level == d - 1) break;
             level++;
         }
+        return root;
+    }
+}
+
+
+/**
+ * Other's Iteration (BFS) solution
+ *
+ * Time: O(2^d). But O(n) in worst case.
+ * Space: O(2^d)
+ */
+class Solution {
+    public TreeNode addOneRow(TreeNode root, int v, int d) {
+        if (d == 1) {
+            TreeNode newroot = new TreeNode(v);
+            newroot.left = root;
+            return newroot;
+        }
+        LinkedList<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        for (int i = 0; i < d-2; i++) {
+            int size = queue.size();
+            for (int j = 0; j < size; j++) {
+                TreeNode t = queue.poll();
+                if (t.left != null) queue.add(t.left);
+                if (t.right != null) queue.add(t.right);
+            }
+        }
+        while (!queue.isEmpty()) {
+            TreeNode t = queue.poll();
+            TreeNode tmp = t.left;
+            t.left = new TreeNode(v);
+            t.left.left = tmp;
+            tmp = t.right;
+            t.right = new TreeNode(v);
+            t.right.right = tmp;
+        }
+        return root;
+    }
+}
+
+
+/**
+ * Other's DFS solution with helper function
+ *
+ * Time: O(2^d). But O(n) in worst case.
+ * Space: O(d).
+ */
+class Solution {
+    private void dfs(TreeNode root, int depth, int v, int d) {
+        if (root == null) return;
+        if (depth < d-1) {
+            dfs(root.left, depth+1, v, d);
+            dfs(root.right, depth+1,v, d);
+        } else {
+            TreeNode tmp = root.left;
+            root.left = new TreeNode(v);
+            root.left.left = tmp;
+            tmp = root.right;
+            root.right = new TreeNode(v);
+            root.right.right = tmp;
+        }
+    }
+    public TreeNode addOneRow(TreeNode root, int v, int d) {
+        if (d == 1) {
+            TreeNode newroot = new TreeNode(v);
+            newroot.left = root;
+            return newroot;
+        }
+        dfs(root, 1, v, d);
+        return root;
+    }
+}
+
+
+/**
+ * Other's DFS solution without helper function
+ *
+ * Time: O(2^d). But O(n) in worst case.
+ * Space: O(d).
+ */
+class Solution {
+    public TreeNode addOneRow(TreeNode root, int v, int d) {
+        if (d < 2) {
+            TreeNode newroot = new TreeNode(v);
+            // to use 1 to indicate attach to left node as required, we can also use 0 to indicate attach to right node
+            if (d == 1) newroot.left = root;
+            else newroot.right = root;
+            return newroot;
+        }
+        if (root == null) return null;
+        root.left = addOneRow(root.left, v, d == 2 ? 1 : d-1);
+        root.right = addOneRow(root.right, v, d == 2 ? 0 : d-1);
         return root;
     }
 }
