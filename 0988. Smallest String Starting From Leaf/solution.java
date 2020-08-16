ede3688597897312faf795894fe146daa9712c78
wilwfy <46659986+wilwfy@users.noreply.github.com>
2020-08-16 15:44:17 -0700

    Add oother's solutions of DFS

--- 0988. Smallest String Starting From Leaf/solution.java
+++ 0988. Smallest String Starting From Leaf/solution.java
@@ -34,6 +34,64 @@ class Solution {
 
 
 /**
+ * Other's solution of DFS
+ * 
+ * Time Complexity: O(NlogN).
+ * Space Complexity: O(N).
+ */
+class Solution {
+    public String smallestFromLeaf(TreeNode root) {
+        return dfs(root, "");
+    }
+
+    public String dfs(TreeNode node, String suffix) {
+        if (node == null) return suffix;
+        
+        suffix = (char)('a' + node.val) + suffix;
+        if (node.left == null && node.right == null) {
+            return suffix;
+        }
+        if (node.left == null || node.right == null) {
+            return (node.left == null) ? dfs(node.right, suffix) : dfs(node.left, suffix);
+        }
+        // Noly do the comparasion at node which has both children, because:
+        // string X < string Y
+        // doesn't guarantee
+        // X + a < Y + a
+        // where a is a character. e.g.: 
+        // "ab" < "abab", but "abz" > "ababz"
+        String leftStr = dfs(node.left, suffix);
+        String rightStr = dfs(node.right, suffix);
+        return leftStr.compareTo(rightStr) <= 0 ? leftStr : rightStr;
+    }
+}
+
+
+/**
+ * Another's solution of DFS with global variable
+ * 
+ * Time Complexity: O(NlogN).
+ * Space Complexity: O(N).
+ */
+class Solution {
+    private String ans = "~"; // dummy value '~' > 'z'
+    
+    public String smallestFromLeaf(TreeNode root) {
+        return dfs(root, "");
+    }
+
+    public String dfs(TreeNode node, String suffix) {
+        if (node == null) return suffix; // base case, and in case root is null.
+        suffix = (char)('a' + node.val) + suffix; // prepend current char to the path string from root.
+        if (node.left == null && node.right == null && ans.compareTo(suffix) > 0)
+            ans = suffix; // update ans if node is a leaf.
+        dfs(node.left, suffix); // recursion to the left branch.
+        dfs(node.right, suffix); // recursion to the right branch.
+        return ans;
+    }
+}
+
+/**
  * Definition for a binary tree node.
  * public class TreeNode {
  *     int val;
