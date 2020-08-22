09f44646b26afea2679c41aa2e7edb0bee4c8bc3
wilwfy <46659986+wilwfy@users.noreply.github.com>
2020-08-21 22:58:31 -0700

    Update other's solution

--- 0284. Peeking Iterator/solution.java
+++ 0284. Peeking Iterator/solution.java
@@ -15,34 +15,34 @@ class PeekingIterator implements Iterator<Integer> {
     private Iterator<Integer> iter;
     boolean noSuchElement;
     
-	public PeekingIterator(Iterator<Integer> iterator) {
-	    // initialize any member here.
+    public PeekingIterator(Iterator<Integer> iterator) {
+	// initialize any member here.
         iter = iterator;
         advanceIter();
-	}
+    }
 	
     // Returns the next element in the iteration without advancing the iterator.
-	public Integer peek() {
+    public Integer peek() {
         // you should confirm with interviewer what to return/throw
         // if there are no more values
         return next;
-	}
+    }
 	
-	// hasNext() and next() should behave the same as in the Iterator interface.
-	// Override them if needed.
-	@Override
-	public Integer next() {
+    // hasNext() and next() should behave the same as in the Iterator interface.
+    // Override them if needed.
+    @Override
+    public Integer next() {
         if (noSuchElement)
             throw new NoSuchElementException();
 	    Integer res = next;
         advanceIter();
         return res;
-	}
+    }
 	
-	@Override
-	public boolean hasNext() {
-	    return !noSuchElement;
-	}
+    @Override
+    public boolean hasNext() {
+	return !noSuchElement;
+    }
     
     private void advanceIter() {
         if (iter.hasNext())
