f029dcc724f0ac568b4406feb8f2c7310ead19a3
wilwfy <46659986+wilwfy@users.noreply.github.com>
2020-08-31 00:33:13 -0700

    Add other's solution of Recursion

--- 0395. Longest Substring with At Least K Repeating Characters/solution.java
+++ 0395. Longest Substring with At Least K Repeating Characters/solution.java
@@ -45,3 +45,34 @@ class Solution {
         return max;
     }
 }
+
+
+/**
+ * Other's solution of Recursion
+ *
+ * Intuition
+ * Without looking for the least occurred character, i.e., split the string by any character that doesn't appear for k times.
+ *
+ */
+class Solution {
+    public int longestSubstring(String s, int k) {
+        int n = s.length();
+        if (n == 0 || n < k) return 0;
+        if (k == 1) return n;
+        int[] counts = new int[26];
+        for (char c: s.toCharArray()) counts[c - 'a']++;
+        boolean valid = true;
+        char badchar = 0;
+        for (int i = 0; i < 26; i++) {
+            if (counts[i] > 0 && counts[i] < k) {
+                badchar = (char)(i + 'a');
+                break;
+            }
+        }
+        if (badchar == 0) return n;
+        String[] subs = s.split(String.valueOf(badchar));
+        int res = 0;
+        for (String sub: subs) res = Math.max(res, longestSubstring(sub, k));
+        return res;
+    }
+}
