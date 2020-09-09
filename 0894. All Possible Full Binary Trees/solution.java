d658282385df8b9406dc39685d0242e725ba712d
wilwfy <46659986+wilwfy@users.noreply.github.com>
2020-09-08 23:12:09 -0700

    Add other's solutions of Divide-and-conquer

---
+++ 0894. All Possible Full Binary Trees/solution.java
@@ -0,0 +1,92 @@
+/**
+ * Other's solution of Divide-and-conquer
+ *
+ * Recursively found the possible children of each node at each level. At every level
+ * subtract 1 from N since this node counts as a node. (N is given as 1 to 50) If N == 1,
+ * return a node with no children. For every i where i is the number of left children,
+ * call allPossibleFBT(i) and allPossibleFBT(N-i) for the remaining children on the opposite
+ * side. Then, iterate through all possible combinations of children setting the current node's
+ * left and right children, and add it to the result list.
+ */
+class Solution {
+    public List<TreeNode> allPossibleFBT(int N) {
+        List<TreeNode> res = new ArrayList<>();
+        if (N == 1) {
+            res.add(new TreeNode(0));
+            return res;
+        }
+        
+        N--;
+        for (int i = 1; i < N; i += 2) {
+            List<TreeNode> left = allPossibleFBT(i);
+            List<TreeNode> right = allPossibleFBT(N - i);
+            int cnt = 1;
+            //System.out.println("======== i = " + i + ", N - i = " + (N-i) + " =========");
+            for (TreeNode nl: left) {
+                for (TreeNode nr: right) {
+                    //System.out.println("cnt = " + cnt);
+                    TreeNode cur = new TreeNode(0);
+                    cur.left = nl;
+                    cur.right = nr;
+                    res.add(cur);
+                    cnt++;
+                }
+            }
+        }
+        return res;
+    }
+}
+
+
+/**
+ * Other's solution of Divide-and-conquer with cache
+ *
+ * We can also add a cache so we don't repeat N values, and return an empty set if N is even
+ * because we can't make a full binary tree with an even number of nodes. (Don't have to create
+ * a copy, we can use the same list).
+ */
+class Solution {
+    Map<Integer,List<TreeNode>> cache = new HashMap<>();
+    
+    public List<TreeNode> allPossibleFBT(int N) {
+        List<TreeNode> res = new ArrayList<>();
+        if (N % 2 == 0) return res;
+        if (cache.containsKey(N)) return cache.get(N);
+        if (N == 1) {
+            res.add(new TreeNode(0));
+            return res;
+        }
+        
+        N = N - 1;
+        for (int i = 1; i < N; i += 2) {
+            List<TreeNode> left = allPossibleFBT(i);
+            List<TreeNode> right = allPossibleFBT(N-i);
+            for (TreeNode nl: left) {
+                for (TreeNode nr:right) {
+                    TreeNode cur = new TreeNode(0);
+                    cur.left = nl;
+                    cur.right = nr;
+                    res.add(cur);
+                }
+            }
+        }
+        cache.put(N, res);
+        return res;
+    }
+}
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
