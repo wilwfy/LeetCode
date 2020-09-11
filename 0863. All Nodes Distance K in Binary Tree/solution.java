527873a22655232d27e65fdb6d3413bd03abb1a0
wilwfy <46659986+wilwfy@users.noreply.github.com>
2020-09-11 00:46:28 -0700

    Add other's DFS solutions with or without HashMap

--- 0863. All Nodes Distance K in Binary Tree/solution.java
+++ 0863. All Nodes Distance K in Binary Tree/solution.java
@@ -76,3 +76,123 @@ class Solution {
 }
 
 
+/**
+ * Other's DFS with HashMap
+ * 
+ * Intuition
+ * 
+ * As we know, if the distance from a node to target node is k, the distance from its child to
+ * the target node is k + 1 unless the child node is closer to the target node which means the
+ * target node is in it's subtree.
+ * 
+ * To avoid this situation, we need to travel the tree first to find the path from root to target, to:
+ *   - store the value of distance in hashamp from the all nodes in that path to target
+ * 
+ * Then we can easily use dfs to travel the whole tree. Every time when we meet a treenode which has
+ * already stored in map, use the stored value in hashmap instead of the value from its parent node.
+ */
+class Solution {
+    Map<TreeNode, Integer> map; // key is node, value is the distance to the target node
+    
+    public List<Integer> distanceK(TreeNode root, TreeNode target, int K) {
+        map = new HashMap<>();
+        List<Integer> res = new ArrayList<>();
+        find(root, target);
+        dfs(root, K, map.get(root), res);
+        return res;
+    }
+    
+    // find target node first and store the distance in that path that we could use it later directly
+    private int find(TreeNode root, TreeNode target) {
+        if (root == null) return -1;
+        if (root == target) {
+            map.put(root, 0);
+            return 0;
+        }
+        int left = find(root.left, target);
+        if (left >= 0) {
+            map.put(root, left + 1);
+            return left + 1;
+        }
+        int right = find(root.right, target);
+        if (right >= 0) {
+            map.put(root, right + 1);
+            return right + 1;
+        }
+        return -1;
+    }
+    
+    private void dfs(TreeNode root, int K, int distance, List<Integer> res) {
+        if (root == null) return;
+        if (map.containsKey(root)) distance = map.get(root);
+        if (distance == K) res.add(root.val);
+        dfs(root.left, K, distance + 1, res);
+        dfs(root.right, K, distance + 1, res);
+    }
+}
+
+
+/**
+ * Other's DFS without HashMap
+ * 
+ * Intuition
+ * same idea, combine two recursive function
+ */
+class Solution {
+    Map<TreeNode, Integer> map; // key is node, value is the distance to the target node
+    
+    public List<Integer> distanceK(TreeNode root, TreeNode target, int K) {
+        List<Integer> res = new ArrayList<>();
+        if (K == 0) {
+            res.add(target.val);
+        } else {
+            dfs(root, target.val, K, 0, res);
+        }
+        return res;
+    }
+       
+    private int dfs(TreeNode node, int target, int K, int depth, List<Integer> res) {
+        if (node == null) return 0;
+        if (depth == K) {
+            res.add(node.val);
+            return 0;
+        }
+        int left, right;
+        if (node.val == target || depth > 0) {
+            left = dfs(node.left, target, K, depth + 1, res);
+            right = dfs(node.right, target, K, depth + 1, res);
+        } else {
+            left = dfs(node.left, target, K, depth, res);
+            right = dfs(node.right, target, K, depth, res);
+        }
+        
+        if (node.val == target) return 1;
+        
+        if (left == K || right == K) {
+            res.add(node.val);
+            return 0;
+        }
+        
+        if (left > 0) {
+            dfs(node.right, target, K, left + 1, res);
+            return left + 1;
+        }
+        
+        if (right > 0) {
+            dfs(node.left, target, K, right + 1, res);
+            return right + 1;
+        }
+        
+        return 0;
+    }
+}
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
