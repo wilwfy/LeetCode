f803b9817901df8ac2ab3aebe4cc85ab30ce6dfe
wilwfy <46659986+wilwfy@users.noreply.github.com>
2020-09-11 20:46:35 -0700

    Add other's solution with Prefix and Suffix Product

--- 0152. Maximum Product Subarray/solution.java
+++ 0152. Maximum Product Subarray/solution.java
@@ -74,3 +74,25 @@ class Solution {
         return max;
     }
 }
+
+
+/**
+ * Other's solution with Prefix and Suffix Product
+ *
+ * Time: O(n)
+ * Space: O(1)
+ */
+class Solution {
+    public int maxProduct(int[] nums) {
+        // Calculate prefix product in nums.
+        // Calculate suffix product in nums.
+        // Return the max.
+        int n = nums.length, res = nums[0], l = 0, r = 0;
+        for (int i = 0; i < n; i++) {
+            l = (l == 0 ? 1 : l) * nums[i];
+            r = (r == 0 ? 1 : r) * nums[n - 1 - i];
+            res = Math.max(res, Math.max(l, r));
+        }
+        return res;
+    }
+}
