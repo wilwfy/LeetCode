28cec6242bd6dab82ef4dbf5f308529a3ff8550a
wilwfy <46659986+wilwfy@users.noreply.github.com>
2020-09-13 12:25:09 -0700

    Add other's solution of Recurrence

--- 0338. Counting Bits/solution.java
+++ 0338. Counting Bits/solution.java
@@ -30,10 +30,25 @@ class Solution {
         int[] oneCnt = new int[num+1];
         for (int i = 0; i <= num; i++) {
             if (i%2 == 0)
-                oneCnt[i] = i != 2 ? oneCnt[i>>>1] : 1;
-            else
+                oneCnt[i] = i != 2 ? oneCnt[i>>>1] : 1; // The operator >>> is 'zero fill right shift'.
+            else                                        // If A is 00111100, then A >>> 2 will get 00001111, but A >> 2 will get 1111
                 oneCnt[i] = i > 2 ? oneCnt[i-1] + 1 : 1;
         }
         return oneCnt;
+    }
+}
+
+
+/**
+ * Other's solution of Recurrence
+ *
+ * An easy recurrence for this problem is f[i] = f[i / 2] + i % 2.
+ */
+class Solution {
+    public int[] countBits(int num) {
+        int[] f = new int[num + 1];
+        for (int i = 1; i <= num; i++)
+            f[i] = f[i >> 1] + (i & 1);
+        return f;
     }
 }
