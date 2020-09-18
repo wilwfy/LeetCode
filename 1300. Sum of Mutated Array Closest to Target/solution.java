527ea86484715dea9fe7d7aa79c533159ca8a47f
wilwfy <46659986+wilwfy@users.noreply.github.com>
2020-09-17 23:58:54 -0700

    Add other's solution of Binary Search

--- 1300. Sum of Mutated Array Closest to Target/solution.java
+++ 1300. Sum of Mutated Array Closest to Target/solution.java
@@ -46,3 +46,47 @@ class Solution {
         return res;
     }
 }
+
+
+/**
+ * Other's solution of Binary Search
+ *
+ * The value we are looking for is somewhere between 1 and maxValue (m).
+ * Now use Binary search to speed up the process.
+ * 
+ * 1. go up if the sum is too small
+ * 2. go down if the sum is too big
+ * When we are done with binary search, l and r are equal, but it might happen that we
+ * have not exactly reached the target.
+ * Check if l-1 (should get us below the target) leads to the sum closer to the target.
+ *
+ * Time: O(logN)
+ * Space: O(1)
+ */
+class Solution {
+    public int findBestValue(int[] arr, int target) {
+        int l, r, mi, s = 0, m = -1;
+        for (int v: arr) {
+		    s += v;
+			m = Math.max(m, v);
+		}
+
+        if (s <= target) return m; // return the max value since we will keep all nums as is
+
+        for (l = 1, r = m; l < r;) {
+            mi = (l + r) / 2;
+            s = 0;
+            for (int v: arr) s += (v > mi) ? mi : v;
+            if (s >= target) r = mi;
+            else l = mi + 1;
+        }
+        // check if we are 1 step off the target 
+        int s1 = 0, s2 = 0;
+        for (int v: arr) {
+            s1 += (v > l) ? l : v;
+            s2 += (v > l - 1) ? (l - 1) : v;
+        }
+        
+        return (Math.abs(s2 - target) <= Math.abs(s1 - target)) ? l - 1 : l;
+    }
+}
