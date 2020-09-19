6d6afb6aedc56329dc099046c55df126dd344179
wilwfy <46659986+wilwfy@users.noreply.github.com>
2020-09-19 14:24:22 -0700

    Update solution.java

--- 1291. Sequential Digits/solution.java
+++ 1291. Sequential Digits/solution.java
@@ -39,3 +39,51 @@ class Solution {
         return res;
     }
 }
+
+
+/**
+ * Other's solution with Sort
+ */
+class Solution {
+    public List<Integer> sequentialDigits(int low, int high) {
+        List<Integer> res = new ArrayList<>();
+        for (int digit = 1; digit < 9; ++digit) {
+            int next = digit;
+			int n = next;
+            while (n <= high && next < 10) {
+                if (n >= low) {
+                    res.add(n);
+                }
+				next++;
+                int n1 = n * 10 + next;
+				// check overflow
+                if (n1 > n)
+                    n = n1;
+                else // Integer overflow.
+                    break;
+            }
+        }
+        Collections.sort(res);
+        return res;        
+    }
+}
+
+
+/**
+ * Other's solution with Substring
+ */
+class Solution {
+    public List<Integer> sequentialDigits(int low, int high) {
+        List<Integer> result = new ArrayList();
+        String s = "123456789";
+        for(int l = 2; l <= s.length(); l++) {
+            for(int j = 0; j <= s.length() - l; j++) {
+                int num = Integer.parseInt(s.substring(j, j + l));
+                if(num > high) return result;
+                if(num >= low) result.add(num);
+            }
+        }
+        
+        return result;
+    }
+}
