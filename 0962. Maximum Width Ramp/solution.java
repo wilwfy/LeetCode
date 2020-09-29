99b224fb7612f297100c7a772825c5722a2fe23e
wilwfy <46659986+wilwfy@users.noreply.github.com>
2020-09-28 17:25:39 -0700

    Add official solution of Binary Search

--- 0962. Maximum Width Ramp/solution.java
+++ 0962. Maximum Width Ramp/solution.java
@@ -30,3 +30,60 @@ class Solution {
         return res;
     }
 }
+
+
+/**
+ * Official solution of Binary Search Candidates
+ *
+ * Intuition
+ * 
+ * Consider i in decreasing order. We want to find the largest j with A[j] >= A[i]
+ * if it exists.
+ * 
+ * Thus, the candidates for j are decreasing: if there is j1 < j2 and A[j1] <= A[j2]
+ * then we strictly prefer j2.
+ * 
+ * Algorithm
+ * 
+ * Let's keep a list of these candidates j. For example, with A = [0,8,2,7,5], the
+ * candidates for i = 0 would be candidates = [(v=5, i=4), (v=7, i=3), (v=8, i=1)].
+ * We keep the list of candidates in decreasing order of i and increasing order of v.
+ * 
+ * Now we can binary search to find the largest j with A[j] >= A[i]: it's the first
+ * one in this list of candidates with v >= A[i].
+ *
+ * Time Complexity: O(NlogN), where N is the length of A.
+ * Space Complexity: O(N).
+ */
+import java.awt.Point;
+
+class Solution {
+    public int maxWidthRamp(int[] A) {
+        int N = A.length;
+
+        int ans = 0;
+        List<Point> candidates = new ArrayList();
+        candidates.add(new Point(A[N-1], N-1));
+
+        // candidates: i's decreasing, by increasing value of A[i]
+        for (int i = N-2; i >= 0; --i) {
+            // Find largest j in candidates with A[j] >= A[i]
+            int lo = 0, hi = candidates.size();
+            while (lo < hi) {
+                int mi = lo + (hi - lo) / 2;
+                if (candidates.get(mi).x < A[i])
+                    lo = mi + 1;
+                else
+                    hi = mi;
+            }
+
+            if (lo < candidates.size()) {
+                int j = candidates.get(lo).y;
+                ans = Math.max(ans, j - i);
+            } else {
+                candidates.add(new Point(A[i], i));
+            }
+        }
+        return ans;
+    }
+}
