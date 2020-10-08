8d8c86ac2366985bacead4386b0c222e2af51040
wilwfy <46659986+wilwfy@users.noreply.github.com>
2020-10-07 18:45:16 -0700

    Add official solution of Dynamic Programming

--- 0877. Stone Game/solution.java
+++ 0877. Stone Game/solution.java
@@ -54,3 +54,55 @@ class Solution {
         return memo[l][r][ID];
     }
 }
+
+
+/**
+ * Official solution of Dynamic Programming
+ * 
+ * Intuition
+ * 
+ * Let's change the game so that whenever Lee scores points, it deducts from Alex's score instead.
+ * 
+ * Let dp(i, j) be the largest score Alex can achieve where the piles remaining are piles[i], piles[i+1],
+ * ..., piles[j]. This is natural in games with scoring: we want to know what the value of each position
+ * of the game is.
+ * 
+ * We can formulate a recursion for dp(i, j) in terms of dp(i+1, j) and dp(i, j-1), and we can use dynamic
+ * programming to not repeat work in this recursion. (This approach can output the correct answer, because
+ * the states form a DAG (directed acyclic graph).)
+ * 
+ * Algorithm
+ * 
+ * When the piles remaining are piles[i], piles[i+1], ..., piles[j], the player who's turn it is has at most 2 moves.
+ * 
+ * The person who's turn it is can be found by comparing j-i to N modulo 2.
+ * 
+ * If the player is Alex, then she either takes piles[i] or piles[j], increasing her score by that amount. Afterwards,
+ * the total score is either piles[i] + dp(i+1, j), or piles[j] + dp(i, j-1); and we want the maximum possible score.
+ * 
+ * If the player is Lee, then he either takes piles[i] or piles[j], decreasing Alex's score by that amount. Afterwards,
+ * the total score is either -piles[i] + dp(i+1, j), or -piles[j] + dp(i, j-1); and we want the minimum possible score.
+ *
+ * Time Complexity: O(N^2), where N is the number of piles.
+ * Space Complexity: O(N^2), the space used storing the intermediate results of each subgame.
+ */
+class Solution {
+    public boolean stoneGame(int[] piles) {
+        int N = piles.length;
+        
+        // dp[i+1][j+1] = the value of the game [piles[i], ..., piles[j]].
+        int[][] dp = new int[N+2][N+2];
+        for (int size = 1; size <= N; ++size)
+            for (int i = 0; i + size <= N; ++i) {
+                int j = i + size - 1;
+                int parity = (j + i + N) % 2; // j - i - N; but +x = -x (mod 2)
+                
+                if (parity == 1)
+                    dp[i+1][j+1] = Math.max(piles[i] + dp[i+2][j+1], piles[j] + dp[i+1][j]);
+                else
+                    dp[i+1][j+1] = Math.min(-piles[i] + dp[i+2][j+1], -piles[j] + dp[i+1][j]);
+            }
+        
+        return dp[1][N] > 0;
+    }
+}
