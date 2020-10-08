258cd3fea70094e2adc144225e4ea57e899ed3ca
wilwfy <46659986+wilwfy@users.noreply.github.com>
2020-10-07 19:18:24 -0700

    Add other's solution with Dummy Node

--- 0061. Rotate List/solution.java
+++ 0061. Rotate List/solution.java
@@ -89,6 +89,47 @@ public ListNode rotateRight(ListNode head, int k) {
 
 
 /**
+ * Other's solution with Dummy Node
+ *
+ * Explanation
+ * Since k may be a large number compared to the length of list. So we need to know the length of
+ * linked list. After that, move the list after the (l-n%l)th node to the front to finish the rotation.
+ * 
+ * Ex: {1,2,3} k=2 Move the list after the 1st node to the front
+ * 
+ * Ex: {1,2,3} k=5, In this case Move the list after (3-5%3=1)st node to the front.
+ * 
+ * So the code has three parts.
+ * 
+ * Get the length
+ * 
+ * Move to the (l-k%l)th node
+ * 
+ * 3)Do the rotation
+ */
+class Solution {
+    public ListNode rotateRight(ListNode head, int k) {
+        if (head == null || head.next == null) return head;
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        ListNode fast = dummy, slow = dummy;
+	    
+        int i;
+        for (i = 0; fast.next != null; i++)// Get the total length 
+        	fast = fast.next;
+        
+        for (int j = i - k % i; j > 0; j--) // Get the i-n%i th node
+        	slow = slow.next;
+        
+        fast.next = dummy.next; // Do the rotation
+        dummy.next = slow.next;
+        slow.next = null;
+        
+        return dummy.next;
+    }
+}
+
+/**
  * Definition for singly-linked list.
  * public class ListNode {
  *     int val;
