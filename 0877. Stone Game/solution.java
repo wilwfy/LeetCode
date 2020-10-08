8a3a5d55857bc1891acbddae3e135043f5176140
wilwfy <46659986+wilwfy@users.noreply.github.com>
2020-10-07 19:07:53 -0700

    Add other's solutions of DP

--- 0877. Stone Game/solution.java
+++ 0877. Stone Game/solution.java
@@ -106,3 +106,42 @@ class Solution {
         return dp[1][N] > 0;
     }
 }
+
+
+/**
+ * Other's solution of 2D DP
+ *
+ * dp[i][j] means the biggest number of stones you can get more than opponent picking piles in piles[i] ~ piles[j].
+ * You can first pick piles[i] or piles[j].
+ * 
+ * If you pick piles[i], your result will be piles[i] - dp[i + 1][j]
+ * If you pick piles[j], your result will be piles[j] - dp[i][j - 1]
+ * So we get:
+ * dp[i][j] = max(piles[i] - dp[i + 1][j], piles[j] - dp[i][j - 1])
+ * We start from smaller subarray and then we use that to calculate bigger subarray.
+ */
+class Solution {
+    public boolean stoneGame(int[] piles) {
+        int n = piles.length;
+        int[][] dp = new int[n][n];
+        for (int i = 0; i < n; i++) dp[i][i] = piles[i];
+        for (int d = 1; d < n; d++)
+            for (int i = 0; i < n - d; i++)
+                dp[i][i + d] = Math.max(piles[i] - dp[i + 1][i + d], piles[i + d] - dp[i][i + d - 1]);
+        return dp[0][n - 1] > 0;
+    }
+}
+
+
+/**
+ * Other's solution of 1D DP
+ */
+class Solution {
+    public boolean stoneGame(int[] piles) {
+        int[] dp = Arrays.copyOf(piles, piles.length);;
+        for (int d = 1; d < piles.length; d++)
+            for (int i = 0; i < piles.length - d; i++)
+                dp[i] = Math.max(piles[i] - dp[i + 1], piles[i + d] - dp[i]);
+        return dp[0] > 0;
+    }
+}
