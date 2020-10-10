523f765c4c7cc1a6595b3e88443c5fb032826c6c
wilwfy <46659986+wilwfy@users.noreply.github.com>
2020-10-10 11:45:48 -0700

    Update other's solution to avoid overflow

--- 0452. Minimum Number of Arrows to Burst Balloons/solution.java
+++ 0452. Minimum Number of Arrows to Burst Balloons/solution.java
@@ -31,12 +31,15 @@ class Solution {
     public int findMinArrowShots(int[][] points) {
         if (points.length == 0) return 0;
 
-        // Sort by end
-        Arrays.sort(points, (p1, p2) -> p1[1] - p2[1]);
+        // Sort by end, use logic operation not abstraction to avoid overflow in case like below:
+        // [[-2147483646,-2147483645],[2147483646,2147483647]]
+        Arrays.sort(points, (p1, p2) -> (p1[1] == p2[1]) ? 0 : ((p1[1] < p2[1]) ? -1 : 1));
+        //Arrays.sort(points, (p1, p2) -> p1[1] - p2[1]);
         
         int cnt = 1;
         int preMinBallonEnd = points[0][1];
         for (int i = 1; i < points.length; i++) {
+            //System.out.println("Min = " + Integer.MIN_VALUE + ", Max = " + Integer.MAX_VALUE);
             if (points[i][0] <= preMinBallonEnd) {
                 continue;
             } else {
