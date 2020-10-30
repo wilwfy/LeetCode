d24f4201900866b4fc95eacbb27f1762fed1122f
wilwfy <46659986+wilwfy@users.noreply.github.com>
2020-10-29 23:50:00 -0700

    Add official solution of Two Pointer

--- 0849. Maximize Distance to Closest Person/solution.java
+++ 0849. Maximize Distance to Closest Person/solution.java
@@ -73,3 +73,47 @@ class Solution {
         return ans;
     }
 }
+
+
+/**
+ * Official solution of Two Pointer
+ * 
+ * Intuition
+ * 
+ * As we iterate through seats, we'll update the closest person sitting to our left,
+ * and closest person sitting to our right.
+ * 
+ * Algorithm
+ * 
+ * Keep track of prev, the filled seat at or to the left of i, and future, the filled
+ * seat at or to the right of i.
+ * 
+ * Then at seat i, the closest person is min(i - prev, future - i), with one exception.
+ * i - prev should be considered infinite if there is no person to the left of seat i,
+ * and similarly future - i is infinite if there is no one to the right of seat i.
+ *
+ * Time Complexity: O(N), where N is the length of seats.
+ * Space Complexity: O(1).
+ */
+class Solution {
+    public int maxDistToClosest(int[] seats) {
+        int N = seats.length;
+        int prev = -1, future = 0;
+        int ans = 0;
+
+        for (int i = 0; i < N; ++i) {
+            if (seats[i] == 1) {
+                prev = i;
+            } else {
+                while (future < N && seats[future] == 0 || future < i)
+                    future++;
+
+                int left = prev == -1 ? N : i - prev;
+                int right = future == N ? N : future - i;
+                ans = Math.max(ans, Math.min(left, right));
+            }
+        }
+
+        return ans;
+    }
+}
