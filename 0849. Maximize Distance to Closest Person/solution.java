dc00cc38d78a29529e6ff817c72d15054d492597
wilwfy <46659986+wilwfy@users.noreply.github.com>
2020-10-29 23:57:17 -0700

    Add other's solution of One Pass

--- 0849. Maximize Distance to Closest Person/solution.java
+++ 0849. Maximize Distance to Closest Person/solution.java
@@ -165,3 +165,29 @@ class Solution {
         return ans;
     }
 }
+
+
+/**
+ * Other's solution of One Pass
+ *
+ * Explanation
+ * last is the index of last seated seat.
+ * Loop on all seats, when we met a people, we count the distance from the last.
+ * The final result = max(distance at the beginning, distance in the middle / 2, distance at the end).
+ * 
+ * Time: O(N).
+ * Space: O(1).
+ */
+class Solution {
+    public int maxDistToClosest(int[] seats) {
+        int res = 0, n = seats.length, last = -1; // last is the index of last seated seat.
+        for (int i = 0; i < n; ++i) {
+            if (seats[i] == 1) {
+                res = last < 0 ? i : Math.max(res, (i - last) / 2);
+                last = i;
+            }
+        }
+        res = Math.max(res, n - last - 1);
+        return res;
+    }
+}
