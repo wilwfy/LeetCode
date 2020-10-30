77cb71ea3ad1c761cd1d3bd487e697c74012c439
wilwfy <46659986+wilwfy@users.noreply.github.com>
2020-10-29 23:47:51 -0700

    Add official solution of Next Array

--- 0849. Maximize Distance to Closest Person/solution.java
+++ 0849. Maximize Distance to Closest Person/solution.java
@@ -29,3 +29,47 @@ class Solution {
         return openEndMax >= closedEndMax ? openEndMax : closedEndMax;
     }
 }
+
+
+/**
+ * Official solution of Next Array
+ *
+ * Intuition
+ * 
+ * Let left[i] be the distance from seat i to the closest person sitting to the left of i.
+ * Similarly, let right[i] be the distance to the closest person sitting to the right of i.
+ * This is motivated by the idea that the closest person in seat i sits a distance
+ * min(left[i], right[i]) away.
+ * 
+ * Algorithm
+ * 
+ * To construct left[i], notice it is either left[i-1] + 1 if the seat is empty, or 0 if it
+ * is full. right[i] is constructed in a similar way.
+ *
+ * Time Complexity: O(N), where N is the length of seats.
+ * Space Complexity: O(N), the space used by left and right.
+ */
+class Solution {
+    public int maxDistToClosest(int[] seats) {
+        int N = seats.length;
+        int[] left = new int[N], right = new int[N];
+        Arrays.fill(left, N);
+        Arrays.fill(right, N);
+
+        for (int i = 0; i < N; ++i) {
+            if (seats[i] == 1) left[i] = 0;
+            else if (i > 0) left[i] = left[i-1] + 1;
+        }
+
+        for (int i = N-1; i >= 0; --i) {
+            if (seats[i] == 1) right[i] = 0;
+            else if (i < N-1) right[i] = right[i+1] + 1;
+        }
+
+        int ans = 0;
+        for (int i = 0; i < N; ++i)
+            if (seats[i] == 0)
+                ans = Math.max(ans, Math.min(left[i], right[i]));
+        return ans;
+    }
+}
