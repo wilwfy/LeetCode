3d9937b3475611776c358876538f997b3124a60c
wilwfy <46659986+wilwfy@users.noreply.github.com>
2021-06-19 12:39:22 -0700

    Add other's solution of Segment Tree

--- 0307. Range Sum Query - Mutable/solution.java
+++ 0307. Range Sum Query - Mutable/solution.java
@@ -35,6 +35,8 @@ class NumArray {
         }
     }
 
+    // Time: O(n)
+    // Space: O(n)
     private void buildTree(int[] nums) {
         for (int i = n, j = 0;  i < 2 * n; i++,  j++)
             tree[i] = nums[j];
@@ -42,6 +44,9 @@ class NumArray {
             tree[i] = tree[i * 2] + tree[i * 2 + 1];
     }
     
+    // Time: O(logn). Because there are a few tree nodes with range that include i th array
+    //                element, one on each level. There are log(n) levels.
+    // Space: O(1)
     void update(int pos, int val) {
         pos += n;
         tree[pos] = val;
@@ -59,6 +64,11 @@ class NumArray {
         }
     }
     
+    // Time: O(logn). Because on each iteration of the algorithm we move one level up, either
+    //                to the parent of the current node or to the next sibling of parent to the
+    //                left or right direction till the two boundaries meet. In the worst-case
+    //                scenario this happens at the root after log(n) iterations of the algorithm.
+    // Space: O(1)
     public int sumRange(int l, int r) {
         // get leaf with value 'l'
         l += n;
@@ -78,6 +88,80 @@ class NumArray {
             r /= 2;
         }
         return sum;
+    }
+}
+
+
+
+/**
+ * Other's solution of Segment Tree
+ */
+class NumArray {
+    class segmentNode{
+        int start;
+        int end;
+        int sum;
+        segmentNode left;
+        segmentNode right;
+        public segmentNode(int start,int end){
+            this.start = start;
+            this.end = end;
+            this.left = null;
+            this.right = null;
+            this.sum = 0;
+        }
+    }
+    
+    segmentNode node;
+    int[] nums;
+    public NumArray(int[] nums) {
+        this.nums = new int[nums.length];
+        for(int i=0;i<nums.length;i++)
+            this.nums[i]=nums[i];
+        this.node = buildtree(nums,0,nums.length-1);
+    }
+    
+    void update(int i, int val) {
+        int diff = nums[i]-val;
+        nums[i]=val;
+        if(node!=null)
+            nupdate(node,i,diff);
+    }
+    
+    public int sumRange(int i, int j) {
+        return node==null?0:sum(node,i,j);
+    }
+    
+    public segmentNode buildtree(int[] nums,int start,int end){
+        if(start>end) return null;
+        segmentNode root = new segmentNode(start,end);
+        if(start == end)
+            root.sum = nums[start];
+        else{
+            int mid = start + (end-start)/2;
+            root.left = buildtree(nums,start,mid);
+            root.right = buildtree(nums,mid+1,end);
+            root.sum = root.left.sum+root.right.sum;
+        }
+        return root;
+    }
+	
+    public int sum(segmentNode root, int start, int end){
+        // don't need to return range sum until (root.start==start && root.end==end),
+        // when root.start>=start && root.end<=end, then the sum of this node can be
+        // used directly, just return root.sum.
+        if(root==null||start>root.end || end<root.start) return 0;
+        if(start<=root.start && end>=root.end)  return root.sum;
+        int left = sum(root.left,start,end);
+        int right = sum(root.right,start,end);
+        return left+right;
+    }
+	
+    public void nupdate(segmentNode root, int index,int diff){
+        if(root==null||index>root.end||index<root.start) return;
+        if(index>=root.start && index<=root.end) root.sum-=diff;
+        nupdate(root.left,index,diff);
+        nupdate(root.right,index,diff);
     }
 }
 
