844525ecd74a58feb6a1ae6be21fee4b8b777e73
wilwfy <46659986+wilwfy@users.noreply.github.com>
2021-08-16 21:58:56 -0700

    Add other's solution of Caching

--- 0303. Range Sum Query - Immutable/solution.java
+++ 0303. Range Sum Query - Immutable/solution.java
@@ -51,3 +51,25 @@ class NumArray {
         return sum[j + 1] - sum[i];
     }
 }
+
+
+/**
+ * Others solution: Caching
+ */
+class NumArray {   
+    int[] nums;
+    
+    public NumArray(int[] nums) {
+        for(int i = 1; i < nums.length; i++)
+            nums[i] += nums[i - 1];
+        
+        this.nums = nums;
+    }
+    
+    public int sumRange(int left, int right) {
+        if(left == 0)
+            return nums[right];
+        
+        return nums[right] - nums[left - 1];
+    }
+}
